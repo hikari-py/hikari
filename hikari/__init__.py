c50b6634bd8f123a6a3b913a3ac495e931c759ea
nekoka.tt <nekoka.tt@outlook.com>
2019-05-01 20:21:38 +0000

    Closes #1

--- hikari/__init__.py
+++ hikari/__init__.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+A lightweight, flexible, customizable Discord API wrapper for Python.
+"""
 
+__author__ = "Nekoka.tt"
+__copyright__ = f"© 2019 {__author__}"
+__license__ = "MIT"
 __version__ = "0.0.1a"
+
+__ci__ = "https://gitlab.com/nekoka.tt/hikari/pipelines"
+__url__ = "https://gitlab.com/nekoka.tt/hikari"
+
+__contributors__ = {
+    "LunarCoffee"
+}
