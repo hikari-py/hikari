c430facfcf15628c4bffc8dd9113adb70ea8018c
Nekoka.tt <Nekoka.tt@outlook.com>
2019-05-03 21:59:13 +0100

    Added debugging info retrieval utility (thought it was kind of cool)

---
+++ hikari/net/debug.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Provides a simple parser for Discord CGI debugging info. This can be used to determine the data center you are using
+on the host running your code.
+
+This API is not officially documented.
+"""
+import datetime
+import re
+
+from dataclasses import dataclass
+
+import aiohttp
+
+
+@dataclass(frozen=True)
+class DataCenter:
+    """Represents a data center. These are represented by an IATA airport code."""
+    #: Airport code
+    iata_code: str
+    #: Data center location
+    location: str
+    #: Data center airport name
+    airport: str
+    #: Data center country
+    country: str
+
+
+@dataclass(frozen=True)
+class DebugData:
+    """The response provided from Discord's CGI trace."""
+    #: Data center location being used. Determined from an IATA airport code.
+    data_center: DataCenter
+    #: Unknown, possibly some form of correlation ID.
+    fl: str
+    #: Your IP
+    ip: str
+    #: UTC unix timestamp.
+    ts: str
+    #: The host that was hit.
+    h: str
+    #: Scheme used
+    visit_scheme: str
+    #: User agent used
+    uag: str
+    #: Airport code of datacenter.
+    colo: str
+    #: HTTP version used.
+    http: str
+    #: Apparent location
+    loc: str
+    #: TLS/SSL version used.
+    tls: str
+    #: Unknown, possibly the content type of this response.
+    sni: str
+    #: Unknown.
+    warp: str
+
+    @property
+    def timestamp(self) -> datetime.datetime:
+        """The timestamp of the request."""
+        return datetime.datetime.fromtimestamp(float(self.ts), datetime.timezone.utc)
+
+
+async def get_debug_data() -> DebugData:
+    """
+    Query the DiscordApp CDN CGI trace to determine debugging info such as the data center that you are likely using.
+    This will then query `http://airlinecodes.co.uk` to determine the data center location from the provided
+    airport code in this response.
+    """
+    async with aiohttp.request('get', 'https://discordapp.com/cdn-cgi/trace') as resp:
+        resp.raise_for_status()
+        content = await resp.text()
+
+    pairs = {}
+    for line in content.splitlines(False):
+        line = line.strip()
+        if line:
+            k, _, v = line.partition('=')
+            pairs[k] = v
+
+    async with aiohttp.request('post', 'http://www.airlinecodes.co.uk/aptcoderes.asp',
+                               data={'iatacode': pairs['colo']}) as resp:
+        resp.raise_for_status()
+        content = await resp.text()
+
+    location_match = re.search(r'<td.*?>Location:</td>\s*?<td>(.*?)</td>', content, re.I | re.M)
+    airport_match = re.search(r'<td.*?>Airport:</td>\s*?<td>(.*?)</td>', content, re.I | re.M)
+    country_match = re.search(r'<td.*?>Country:</td>\s*?<td>(.*?)</td>', content, re.I | re.M)
+
+    location = location_match and location_match.group(1).strip() or 'Unknown'
+    airport = airport_match and airport_match.group(1).strip() or 'Unknown'
+    country = country_match and country_match.group(1).strip() or 'Unknown'
+
+    pairs['data_center'] = DataCenter(pairs['colo'], location, airport, country)
+    return DebugData(**pairs)
+
+
+if __name__ == '__main__':
+    import asyncio
+    print(asyncio.run(get_debug_data()))
