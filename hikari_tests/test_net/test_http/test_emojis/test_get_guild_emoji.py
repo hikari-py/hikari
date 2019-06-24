47de096ea38aecae27a42bc8d42d15d733fee35d
XLR <tmpod@protonmail.com>
2019-06-24 11:08:07 +0000

    Fixed black-fix tox job
    This job was trying to run Black on the tests dir using the old name.
    Just changed `tests` to `hikari_tests`.

---
+++ hikari_tests/test_net/test_http/test_emojis/test_get_guild_emoji.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import asynctest
+import pytest
+
+
+@pytest.fixture()
+def http_client(event_loop):
+    from hikari_tests.test_net.test_http import ClientMock
+
+    return ClientMock(token="foobarsecret", loop=event_loop)
+
+
+@pytest.mark.asyncio
+async def test_get_guild_emojis(http_client):
+    http_client.request = asynctest.CoroutineMock()
+    await http_client.get_guild_emoji("424242", "404101")
+    http_client.request.assert_awaited_once_with(
+        "get", "/guilds/{guild_id}/emojis/{emoji_id}", guild_id="424242", emoji_id="404101"
+    )
