c0e9e3d8f3a7204601a3a8499b84485baffc52c4
Nekoka.tt <Nekoka.tt@outlook.com>
2019-06-26 17:15:19 +0100

    Fixed formatting and linter issues

--- hikari_tests/test_net/test_http/test_channel/test_edit_channel_permissions.py
+++ hikari_tests/test_net/test_http/test_channel/test_edit_channel_permissions.py
@@ -14,7 +14,7 @@ def http_client(event_loop):
 @pytest.mark.asyncio
 async def test_edit_channel_permissions(http_client):
     http_client.request = asynctest.CoroutineMock()
-    await http_client.edit_channel_permissions("69", "420", allow=192, deny=168, type="member")
+    await http_client.edit_channel_permissions("69", "420", allow=192, deny=168, type_="member")
     http_client.request.assert_awaited_once_with(
         "put",
         "/channels/{channel_id}/permissions/{overwrite_id}",
