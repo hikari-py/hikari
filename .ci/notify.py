7c8f6b3a8b1c3101ec26ae51dd275760e9264223
Nekoka.tt <Nekoka.tt@outlook.com>
2019-06-27 20:14:22 +0100

    Revert "Removed annoying webhook"
    This reverts commit 9e54e9ad5a5b2319c97d98430f0475d146678fdb.

---
+++ .ci/notify.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Notify on Discord via a webhook that a new version has been released to PyPi.
+"""
+import os
+import sys
+import traceback
+
+import requests
+
+
+try:
+    WEBHOOK_URL = os.environ["RELEASE_WEBHOOK"]
+    ENVIRONMENT = os.environ["RELEASE_WEBHOOK_NAME"]
+    COLOUR = os.environ["RELEASE_WEBHOOK_COLOUR"]
+    DESCRIPTION = os.environ["RELEASE_WEBHOOK_DESCRIPTION"]
+    VERSION = sys.argv[1]
+
+    requests.post(
+        WEBHOOK_URL,
+        json={
+            "embeds": [
+                {
+                    "title": f"[{VERSION}] New {ENVIRONMENT} deployment!",
+                    "description": f"Hikari v{VERSION} has just been put into {ENVIRONMENT}.",
+                    "color": int(COLOUR, 16),
+                    "author": {"name": "Nekoka.tt"},
+                    "footer": {"text": DESCRIPTION}
+                }
+            ]
+        }
+    )
+except BaseException:
+    traceback.print_exc()
