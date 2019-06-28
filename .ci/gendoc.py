0306d5cb5f5d1311a64dc4d6f91303742999b1c0
Nekoka.tt <Nekoka.tt@outlook.com>
2019-06-28 19:44:06 +0100

    Final doc-gen tweaks

--- .ci/gendoc.py
+++ .ci/gendoc.py
@@ -1,5 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# Copyright © Nekoka.tt 2019
+#
+# This file is part of Hikari.
+#
+# Hikari is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hikari is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Hikari. If not, see <https://www.gnu.org/licenses/>.
+"""
+Generates module documentation for me. Turns out Sphinx is a pain for doing this in a "simple" way. All the stuff on
+PyPi for doing this has annoying quirks that I don't care for, like not supporting asyncio, or only working on a Tuesday
+or during a full moon, and it is just angering.
+
+Arg1 = package to document
+Arg2 = templates dir for gendoc
+Arg3 = path to write index.rst to
+Arg4 = path to write module rst files to
+"""
 import os
 import sys
 
@@ -9,20 +35,20 @@ import jinja2
 def remove_base_path(base, path):
     base = os.path.abspath(os.path.join(base, ".."))
     path = os.path.abspath(path)
-    truncated = path[len(base):]
+    truncated = path[len(base) :]
     return truncated[1:] if truncated.startswith("/") or truncated.startswith("\\") else truncated
 
 
 def is_valid_python_file(path):
     base = os.path.basename(path)
-    return not base.startswith('__') and (os.path.isdir(path) or base.endswith('.py'))
+    return not base.startswith("__") and (os.path.isdir(path) or base.endswith(".py"))
 
 
 def to_module_name(rel_path, base):
     rel_path = remove_base_path(base, rel_path)
-    bits = rel_path.split('/')
+    bits = rel_path.split("/")
     bits[-1], _ = os.path.splitext(bits[-1])
-    return '.'.join(bits)
+    return ".".join(bits)
 
 
 def iter_all_module_paths_in(base):
@@ -47,20 +73,33 @@ def iter_all_module_paths_in(base):
 def main(*argv):
     base = argv[0]
     modules = [to_module_name(module_path, base) for module_path in sorted(iter_all_module_paths_in(base))]
-    print(f"Found {len(modules)} modules to document:", *modules, sep='\n   - ')
+    print(f"Found {len(modules)} modules to document:", *modules, sep="\n   - ")
 
-    tempalate_dir = argv[1]
+    template_dir = argv[1]
 
-    environment = jinja2.Environment(loader=jinja2.FileSystemLoader(tempalate_dir))
+    with open(os.path.join(template_dir, "index.rst")) as fp:
+        print("Reading", fp.name)
+        index_template = jinja2.Template(fp.read())
 
-    index_template = environment.get_template('index.rst')
-    module_template = jinja2.Template('module.rst')
-    class_template = jinja2.Template('class.rst')
-    function_template = jinja2.Template('function.rst')
-    variable_template = jinja2.Template('variable.rst')
+    index_file = argv[2]
 
-    documentation_path = argv[2]
-    os.makedirs(documentation_path, 0o644, exist_ok=True)
+    with open(index_file, "w") as fp:
+        print("Writing", fp.name)
+        fp.write(index_template.render(modules=modules))
+
+    with open(os.path.join(template_dir, "module.rst")) as fp:
+        print("Reading", fp.name)
+        module_template = jinja2.Template(fp.read())
+
+    documentation_path = argv[3]
+    os.makedirs(documentation_path, 0o1777, exist_ok=True)
+
+    for m in modules:
+        with open(os.path.join(documentation_path, m + ".rst"), "w") as fp:
+            submodules = [sm for sm in modules if sm.startswith(m) and sm != m]
+
+            print("Writing", fp.name)
+            fp.write(module_template.render(module=m, rule=len(m) * "#", submodules=submodules))
 
 
 if __name__ == "__main__":
---
+++ docs/_templates/gendoc/index.rst
@@ -0,0 +1,19 @@
+Hikari documentation
+####################
+
+This is for version |version| -- on PyPi, this is referred to as |release|
+
+Hikari is licensed under the <GNU LGPLv3 `https://www.gnu.org/licenses/lgpl-3.0.en.html`>_.
+
+Packages and submodules
+-----------------------
+
+.. autosummary::
+    :toctree: modules
+
+    {% for m in modules %}{{ m }}
+    {% endfor %}
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
