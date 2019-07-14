fdd66382641b161cdf2280acc70e4aa871676989
Nekoka.tt <Nekoka.tt@outlook.com>
2019-07-14 15:43:19 +0100

    Tidied up status tests a little

--- hikari_tests/test_net/test_service_status.py
+++ hikari_tests/test_net/test_service_status.py
@@ -79,86 +79,59 @@ def patched_request():
         yield router
 
 
-# If dacite can parse the responses, we don't care. Everything else is validated already.
-
-
 @pytest.mark.asyncio
-async def test_get_status(patched_request):
-    assert isinstance(await service_status.get_status(), status_model.Status)
+@pytest.mark.model
+class TestServiceStatus:
+    async def test_get_status(self, patched_request):
+        assert isinstance(await service_status.get_status(), status_model.Status)
 
+    async def test_get_summary(self, patched_request):
+        assert isinstance(await service_status.get_summary(), status_model.Summary)
 
-@pytest.mark.asyncio
-async def test_get_summary(patched_request):
-    assert isinstance(await service_status.get_summary(), status_model.Summary)
+    async def test_get_components(self, patched_request):
+        assert isinstance(await service_status.get_components(), status_model.Components)
 
+    async def test_get_all_incidents(self, patched_request):
+        assert isinstance(await service_status.get_all_incidents(), status_model.Incidents)
 
-@pytest.mark.asyncio
-async def test_get_components(patched_request):
-    assert isinstance(await service_status.get_components(), status_model.Components)
+    async def test_get_unresolved_incidents(self, patched_request):
+        assert isinstance(await service_status.get_unresolved_incidents(), status_model.Incidents)
 
+    async def test_get_all_scheduled_maintenances(self, patched_request):
+        assert isinstance(await service_status.get_all_scheduled_maintenances(), status_model.ScheduledMaintenances)
 
-@pytest.mark.asyncio
-async def test_get_all_incidents(patched_request):
-    assert isinstance(await service_status.get_all_incidents(), status_model.Incidents)
+    async def test_get_upcoming_scheduled_maintenances(self, patched_request):
+        assert isinstance(
+            await service_status.get_upcoming_scheduled_maintenances(), status_model.ScheduledMaintenances
+        )
 
+    async def test_get_active_scheduled_maintenances(self, patched_request):
+        assert isinstance(await service_status.get_active_scheduled_maintenances(), status_model.ScheduledMaintenances)
 
-@pytest.mark.asyncio
-async def test_get_unresolved_incidents(patched_request):
-    assert isinstance(await service_status.get_unresolved_incidents(), status_model.Incidents)
+    async def test_get_active_scheduled_maintenances(self, patched_request):
+        assert isinstance(await service_status.get_active_scheduled_maintenances(), status_model.ScheduledMaintenances)
 
+    async def test_subscribe_email_to_all_incidents(self, patched_request):
+        assert isinstance(await service_status.subscribe_email_to_all_incidents("foo@bar.com"), status_model.Subscriber)
 
-@pytest.mark.asyncio
-async def test_get_all_scheduled_maintenances(patched_request):
-    assert isinstance(await service_status.get_all_scheduled_maintenances(), status_model.ScheduledMaintenances)
+    async def test_subscribe_email_to_incidents(self, patched_request):
+        subscription = await service_status.subscribe_email_to_incident("foo@bar.com", "1a2b3c")
+        assert isinstance(subscription, status_model.Subscriber)
 
+    async def test_subscribe_webhook_to_all_incidents(self, patched_request):
+        assert isinstance(
+            await service_status.subscribe_webhook_to_all_incidents("foo@bar.com", "localhost:8080/lol"),
+            status_model.Subscriber,
+        )
 
-@pytest.mark.asyncio
-async def test_get_upcoming_scheduled_maintenances(patched_request):
-    assert isinstance(await service_status.get_upcoming_scheduled_maintenances(), status_model.ScheduledMaintenances)
+    async def test_subscribe_webhook_to_incidents(self, patched_request):
+        assert isinstance(
+            await service_status.subscribe_webhook_to_incident("foo@bar.com", "localhost:8080/lol", "1a2b3c"),
+            status_model.Subscriber,
+        )
 
+    async def test_unsubscribe(self, patched_request):
+        assert await service_status.unsubscribe_from("1a2b3c") is None
 
-@pytest.mark.asyncio
-async def test_get_active_scheduled_maintenances(patched_request):
-    assert isinstance(await service_status.get_active_scheduled_maintenances(), status_model.ScheduledMaintenances)
-
-
-@pytest.mark.asyncio
-async def test_get_active_scheduled_maintenances(patched_request):
-    assert isinstance(await service_status.get_active_scheduled_maintenances(), status_model.ScheduledMaintenances)
-
-
-@pytest.mark.asyncio
-async def test_subscribe_email_to_all_incidents(patched_request):
-    assert isinstance(await service_status.subscribe_email_to_all_incidents("foo@bar.com"), status_model.Subscriber)
-
-
-@pytest.mark.asyncio
-async def test_subscribe_email_to_incidents(patched_request):
-    subscription = await service_status.subscribe_email_to_incident("foo@bar.com", "1a2b3c")
-    assert isinstance(subscription, status_model.Subscriber)
-
-
-@pytest.mark.asyncio
-async def test_subscribe_webhook_to_all_incidents(patched_request):
-    assert isinstance(
-        await service_status.subscribe_webhook_to_all_incidents("foo@bar.com", "localhost:8080/lol"),
-        status_model.Subscriber,
-    )
-
-
-@pytest.mark.asyncio
-async def test_subscribe_webhook_to_incidents(patched_request):
-    assert isinstance(
-        await service_status.subscribe_webhook_to_incident("foo@bar.com", "localhost:8080/lol", "1a2b3c"),
-        status_model.Subscriber,
-    )
-
-
-@pytest.mark.asyncio
-async def test_unsubscribe(patched_request):
-    assert await service_status.unsubscribe_from("1a2b3c") is None
-
-
-@pytest.mark.asyncio
-async def test_resend_confirmation_email(patched_request):
-    assert await service_status.resend_confirmation_email("1a2b3c") is None
+    async def test_resend_confirmation_email(self, patched_request):
+        assert await service_status.resend_confirmation_email("1a2b3c") is None
