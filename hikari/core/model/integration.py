30cc2144f322ab86443049784e0bdfed8f81d418
tandemdude <olijdthompson@gmail.com>
2019-08-17 22:52:55 +0200

    Populate Integration and IntegrationAccount classes. Create from_dict methods for both. Create test_integration.py. Change how account is casted at a later date, hacked something together that worked however not how it should be done in final.

--- hikari/core/model/integration.py
+++ hikari/core/model/integration.py
@@ -19,20 +19,82 @@
 """
 Account integrations.
 """
-__all__ = ()
+__all__ = ("Integration", "IntegrationAccount")
+
+import typing
+import datetime
+
+from hikari.core.model import base
+from hikari.core.model import user
+from hikari.core.model import model_state
+from hikari.core.utils import transform
+from hikari.core.utils import dateutils
 
 
-class Integration:
-    __slots__ = ()
-
-    @classmethod
-    def from_dict(cls, payload):
-        return NotImplemented
-
-
+@base.dataclass()
 class IntegrationAccount:
-    __slots__ = ()
+    __slots__ = (
+        "_state",
+        "id",
+        "name"
+    )
 
-    @classmethod
-    def from_dict(cls, payload):
-        return NotImplemented
+    _state: typing.Any
+    id: int
+    name: str
+
+    @staticmethod
+    def from_dict(global_state: model_state.AbstractModelState, payload):
+        return IntegrationAccount(
+            _state=global_state,
+            id=transform.get_cast(payload, "id", int),
+            name=payload.get("name")
+        )
+
+
+@base.dataclass()
+class Integration:
+    __slots__ = (
+        "_state",
+        "id",
+        "name",
+        "type",
+        "enabled",
+        "syncing",
+        "role_id",
+        "expire_behavior",
+        "expire_grace_period",
+        "user",
+        "account",
+        "synced_at"
+    )
+
+    _state: typing.Any
+    id: int
+    name: str
+    type: str
+    enabled: bool
+    syncing: bool
+    role_id: int
+    expire_behavior: int
+    expire_grace_period: int
+    user: "user.User"
+    account: IntegrationAccount
+    synced_at: datetime.datetime
+
+    @staticmethod
+    def from_dict(global_state: model_state.AbstractModelState, payload):
+        return Integration(
+            _state=global_state,
+            id=transform.get_cast(payload, "id", int),
+            name=payload.get("name"),
+            type=payload.get("type"),
+            enabled=transform.get_cast(payload, "enabled", bool),
+            syncing=transform.get_cast(payload, "syncing", bool),
+            role_id=transform.get_cast(payload, "role_id", int),
+            expire_behavior=transform.get_cast(payload, "expire_behavior", int),
+            expire_grace_period=transform.get_cast(payload, "expire_grace_period", int),
+            user=global_state.parse_user(payload.get("user")),
+            account=IntegrationAccount.from_dict(global_state, payload.get("account")), # Change this later, slightly hacky way to do it
+            synced_at=transform.get_cast(payload, "synced_at", dateutils.parse_iso_8601_datetime)
+        )
