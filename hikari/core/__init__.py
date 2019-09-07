b898a78ea7a99b7abe933c6bdb103dc085536c2c
Nekokatt <69713762+nekokatt@users.noreply.github.com>
2019-09-07 08:42:59 +0000

    Added rock500 to contributors list [skip ci]

--- hikari/core/__init__.py
+++ hikari/core/__init__.py
@@ -23,7 +23,7 @@ This package contains the core components for this framework. This includes the 
 limiting, and basic client.
 """
 __author__ = "Nekoka.tt"
-__contributors__ = {"LunarCoffee", "raatty", "Tmpod", "Zach", "thomm.o"}
+__contributors__ = {"LunarCoffee", "raatty", "Tmpod", "Zach", "thomm.o", "rock500"}
 __copyright__ = f"© 2019 Nekoka.tt"
 __license__ = "LGPLv3"
 __version__ = "0.0.10"
