05abedc1964eb3f6b8c86ce2eea05bb307ac66ec
Nekoka.tt <Nekoka.tt@outlook.com>
2019-09-11 22:30:12 +0100

    Made initial emoji framework stuff [skip ci]

--- hikari/core/model/emoji.py
+++ hikari/core/model/emoji.py
@@ -21,69 +21,119 @@ Emojis.
 """
 from __future__ import annotations
 
+import abc
 import dataclasses
 import typing
+import unicodedata
 
 from hikari.core.model import base
 from hikari.core.model import model_cache
-from hikari.core.model import user
-from hikari.core.utils import transform
+
+
+class BaseEmoji(abc.ABC):
+    """
+    ABC for any emoji that Discord may present to us.
+    """
+    __slots__ = ()
+
+    @property
+    @abc.abstractmethod
+    def name(self) -> str:
+        """The friendly name of the emoji."""
+
+    @property
+    @abc.abstractmethod
+    def value(self) -> str:
+        """The raw value of the emoji. This is the raw string representation."""
+
+
+class UnicodeEmoji(BaseEmoji, str):
+    """
+    Represents a standard emoji that is part of unicode.
+
+    Examples include \N{OK HAND SIGN} and \N{AUBERGINE}, both of which are one unicode character.
+
+    Other cases include combined characters such as \N{REGIONAL INDICATOR SYMBOL LETTER G} and
+    \N{REGIONAL INDICATOR SYMBOL LETTER B} which combine to make a UK flag emoji on Discord:
+    \N{REGIONAL INDICATOR SYMBOL LETTER G}\N{REGIONAL INDICATOR SYMBOL LETTER B}.
+    """
+    __slots__ = ()
+
+    @property
+    def name(self) -> str:
+        """The official name of the unicode character sequence."""
+        return unicodedata.name(self).lower()
+
+    @property
+    def value(self) -> str:
+        """The actual character string representation of the emoji."""
+        return self
+
+    @property
+    def code_points(self) -> typing.List[int]:
+        """
+        A list of code points. This will be the same length as the length of the emoji with `len()`,
+        and each value will correspond to the ordinal value of the character. This mechanism exists to handle
+        composite characters such as flags and keycapped numbers (`flag_gb` and `nine`, as examples)
+        """
+        return [ord(c) for c in self]
+
+    def __repr__(self):
+        return f"{type(self).__qualname__}(name={self.name!r})"
+
+    def __str__(self):
+        return self
 
 
 @dataclasses.dataclass()
-class Emoji(base.Snowflake):
+class CustomEmoji(BaseEmoji, base.Snowflake):
     """
-    Representation of a custom emoji object.
+    A custom emoji which may or may not contain guild-specific additional information, depending on whether the
+    authorized user is a member in the guild the emoji belongs to or not.
     """
+    __slots__ = ("id", "_name", "animated", "guild_details")
 
-    __slots__ = ("_state", "id", "name", "_role_ids", "_guild_id", "user", "require_colons", "managed", "animated")
+    _name: str
+    animated: bool
+    #: Details about the emoji from the guild it is defined in, if the authorized user is in that guild.
+    #:
+    #: :type: :class:`EmojiGuildDetails` if we are in the same guild, or `None` otherwise.
+    guild_details: typing.Optional[EmojiGuildDetails]
 
-    _state: typing.Any
-    _role_ids: typing.List[int]
+    @property
+    def name(self) -> str:
+        """The name of the emoji only."""
+        return self._name
+
+    @property
+    def value(self) -> str:
+        """The raw emoji mention."""
+        return "<{0}:{1}:{2}>".format("a" if self.animated else "", self._name, self.id)
+
+    @property
+    def require_colons(self) -> bool:
+        # All custom emojis need colons as far as we know.
+        return True
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return (
+            f"{type(self).__qualname__}" 
+            f"(name={self.name!r}, animated={self.animated}, guild_details={self.guild_details})"
+        )
+
+
+@dataclasses.dataclass()
+class EmojiGuildDetails:
+    __slots__ = ("_state", "_guild_id", "_creator_id", "managed")
+    _state: model_cache.AbstractModelCache
     _guild_id: int
-
-    #: The id of the emoji
-    #:
-    #: :type: :class:`int`
-    id: int
-
-    #: The name of the emoji
-    #:
-    #: :type: :class:`str`
-    name: str
-
-    #: The user whom added the emoji
-    #:
-    #: :type: :class:`hikari.core.models.user.User`
-    user: user.User
-
-    #: Whether the emoji should be wrapped in colons or not
-    #:
-    #: :type: :class:`bool`
-    require_colons: bool
-
-    #: Whether the emoji is managed or not as part of some integration, such as Twitch.
-    #:
-    #: :type: :class:`bool`
+    _creator_id: int
     managed: bool
 
-    #: Whether the emoji is animated or not
-    #:
-    #: :type: :class:`bool`
-    animated: bool
 
-    def __init__(self, global_state: model_cache.AbstractModelCache, payload, guild_id: int):
-        """Convert the given payload and state into an object instance."""
-        self._state = global_state
-        self.id = int(payload["id"])
-        self.name = payload["name"]
-        self._role_ids = [global_state.parse_role(role).id for role in payload.get("roles", ())]
-        self._guild_id = guild_id
-        user = payload.get("user")
-        self.user = global_state.parse_user(user) if user is not None else None
-        self.require_colons = payload.get("require_colons", False)
-        self.managed = payload.get("managed", False)
-        self.animated = payload.get("animated", False)
-
-
-__all__ = ["Emoji"]
+__all__ = [
+    "BaseEmoji", "UnicodeEmoji", "CustomEmoji", "EmojiGuildDetails"
+]
