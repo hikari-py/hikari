30870c30cddff033972c0ef9b7d43cf489723912
Nekokatt <3903853-nekokatt@users.noreply.gitlab.com>
2019-11-16 16:57:08 +0000

    Fixed tests that somehow broke in octomerge.

--- tests/hikari/orm/models/test_members.py
+++ tests/hikari/orm/models/test_members.py
@@ -21,6 +21,7 @@ from unittest import mock
 
 import pytest
 
+from hikari.orm import fabric
 from hikari.orm import state_registry
 from hikari.orm.models import guilds
 from hikari.orm.models import members
@@ -29,9 +30,18 @@ from hikari.orm.models import users
 from tests.hikari import _helpers
 
 
+@pytest.fixture()
+def mock_state_registry():
+    return mock.MagicMock(spec_set=state_registry.IStateRegistry)
+
+
+@pytest.fixture()
+def fabric_obj(mock_state_registry):
+    return fabric.Fabric(state_registry=mock_state_registry)
+
+
 @pytest.mark.model
-def test_Member_with_filled_fields():
-    s = mock.MagicMock(spec_set=state_registry.IStateRegistry)
+def test_Member_with_filled_fields(fabric_obj):
     user_dict = {
         "id": "123456",
         "username": "Boris Johnson",
@@ -42,8 +52,8 @@ def test_Member_with_filled_fields():
         "premium_type": 0b1101101,
     }
     guild_obj = _helpers.mock_model(guilds.Guild, id=12345)
-    m = members.Member(
-        s,
+    member_obj = members.Member(
+        fabric_obj,
         guild_obj,
         {
             "nick": "foobarbaz",
@@ -57,20 +67,19 @@ def test_Member_with_filled_fields():
         },
     )
 
-    assert m.nick == "foobarbaz"
-    assert m.joined_at == datetime.datetime(2015, 4, 26, 6, 26, 56, 936000, datetime.timezone.utc)
-    assert m.premium_since == datetime.datetime(2019, 5, 17, 6, 26, 56, 936000, datetime.timezone.utc)
-    assert m.guild is guild_obj
-    s.parse_user.assert_called_with(user_dict)
+    assert member_obj.nick == "foobarbaz"
+    assert member_obj.joined_at == datetime.datetime(2015, 4, 26, 6, 26, 56, 936000, datetime.timezone.utc)
+    assert member_obj.premium_since == datetime.datetime(2019, 5, 17, 6, 26, 56, 936000, datetime.timezone.utc)
+    assert member_obj.guild is guild_obj
+    fabric_obj.state_registry.parse_user.assert_called_with(user_dict)
 
 
 @pytest.mark.model
-def test_Member_with_no_optional_fields():
-    s = mock.MagicMock(spec_set=state_registry.IStateRegistry)
+def test_Member_with_no_optional_fields(fabric_obj):
     user_dict = {"id": "123456", "username": "Boris Johnson", "discriminator": "6969", "avatar": "1a2b3c4d"}
     guild_obj = _helpers.mock_model(guilds.Guild, id=12345)
-    m = members.Member(
-        s,
+    member_obj = members.Member(
+        fabric_obj,
         guild_obj,
         {
             "roles": ["11111", "22222", "33333", "44444"],
@@ -79,52 +88,60 @@ def test_Member_with_no_optional_fields():
         },
     )
 
-    assert m.nick is None
-    assert m.joined_at == datetime.datetime(2015, 4, 26, 6, 26, 56, 936000, datetime.timezone.utc)
-    assert m.premium_since is None
-    assert m.guild is guild_obj
-    s.parse_user.assert_called_with(user_dict)
+    assert member_obj.nick is None
+    assert member_obj.joined_at == datetime.datetime(2015, 4, 26, 6, 26, 56, 936000, datetime.timezone.utc)
+    assert member_obj.premium_since is None
+    assert member_obj.guild is guild_obj
+    fabric_obj.state_registry.parse_user.assert_called_with(user_dict)
 
 
 @pytest.mark.model
-def test_Member_update_state():
-    # We have faff mocking the delegate neatly, so whatever. Hacks also work.
+def test_Member_update_state(fabric_obj):
     role_one = _helpers.mock_model(roles.Role)
     role_two = _helpers.mock_model(roles.Role)
     role_three = _helpers.mock_model(roles.Role)
 
     role_objs = [role_one, role_two, role_three]
 
-    class MockMember:
-        _state = mock.MagicMock(side_effects=[role_one, role_two, role_three])
-        _user = mock.MagicMock()
+    user_dict = {"id": "123456", "username": "Boris Johnson", "discriminator": "6969", "avatar": "1a2b3c4d"}
+    guild_obj = _helpers.mock_model(guilds.Guild, id=12345)
+    member_obj = members.Member(
+        fabric_obj,
+        guild_obj,
+        {
+            "roles": ["11111", "22222", "33333", "44444"],
+            "joined_at": "2015-04-26T06:26:56.936000+00:00",
+            "user": user_dict,
+        },
+    )
 
-    m = mock.MagicMock(wraps=MockMember())
-    members.Member.update_state(m, role_objs, "potato")
-    assert m.nick == "potato"
-    assert m.roles == role_objs
-
-
-@pytest.mark.model
-def test_Member_user_accessor():
-    u = mock.MagicMock(spec=users.User)
-    g = mock.MagicMock(spec=guilds.Guild)
-    s = mock.MagicMock(spec_set=state_registry.IStateRegistry)
-    # Member's state is delegated to the inner user.
-    u._state = s
-    s.parse_user = mock.MagicMock(return_value=u)
-    s.get_guild_by_id = mock.MagicMock(return_value=g)
-    m = members.Member(s, 1234, {"joined_at": "2019-05-17T06:26:56.936000+00:00", "user": u})
-    assert m.user is u
+    member_obj.update_state(role_objs, "potato")
+    assert member_obj.nick == "potato"
+    assert member_obj.roles == role_objs
 
 
 @pytest.mark.model
-def test_Member_guild_accessor():
-    u = mock.MagicMock(spec=users.User)
-    g = mock.MagicMock(spec=guilds.Guild)
-    s = mock.MagicMock(spec_set=state_registry.IStateRegistry)
+def test_Member_user_accessor(fabric_obj):
+    user_obj = mock.MagicMock(spec_set=users.User)
+    guild_obj = mock.MagicMock(spec_set=guilds.Guild)
     # Member's state is delegated to the inner user.
-    u._state = s
-    s.parse_user = mock.MagicMock(return_value=u)
-    m = members.Member(s, g, {"joined_at": "2019-05-17T06:26:56.936000+00:00", "user": u, "guild_id": 1234})
-    assert m.guild is g
+    user_obj._fabric = fabric_obj
+    fabric_obj.state_registry.parse_user = mock.MagicMock(return_value=user_obj)
+    fabric_obj.state_registry.get_guild_by_id = mock.MagicMock(return_value=guild_obj)
+    member_obj = members.Member(
+        fabric_obj, guild_obj, {"joined_at": "2019-05-17T06:26:56.936000+00:00", "user": user_obj}
+    )
+    assert member_obj.user is user_obj
+
+
+@pytest.mark.model
+def test_Member_guild_accessor(fabric_obj):
+    user_obj = mock.MagicMock(spec_set=users.User)
+    guild_obj = mock.MagicMock(spec_set=guilds.Guild)
+    # Member's state is delegated to the inner user.
+    user_obj._fabric = fabric
+    fabric_obj.state_registry.parse_user = mock.MagicMock(return_value=user_obj)
+    m = members.Member(
+        fabric_obj, guild_obj, {"joined_at": "2019-05-17T06:26:56.936000+00:00", "user": user_obj, "guild_id": 1234}
+    )
+    assert m.guild is guild_obj
