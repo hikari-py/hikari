11b8c495fea71d2f3c93975dd104df649acd46c5
Nekokatt <3903853-nekokatt@users.noreply.gitlab.com>
2020-05-12 20:42:53 +0000

    Update black.nox.py to mute lgtm issue [skip ci]

--- ci/black.nox.py
+++ ci/black.nox.py
@@ -17,8 +17,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Hikari. If not, see <https://www.gnu.org/licenses/>.
 """Black code-style jobs."""
-import subprocess
-
 from ci import nox
 
 
