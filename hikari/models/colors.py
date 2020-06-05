0ffeeb0525bf0ec901a3847711bd3aa337b8181d
Nekokatt <3903853-nekokatt@users.noreply.gitlab.com>
2020-06-05 14:33:38 +0100

    Color documentation fixes.

--- hikari/models/colors.py
+++ hikari/models/colors.py
@@ -47,60 +47,68 @@ class Color(int):
     --------
     Examples of conversions to given formats include:
 
-        >>> c = Color(0xFF051A)
-        Color(r=0xff, g=0x5, b=0x1a)
-        >>> hex(c)
-        0xff051a
-        >>> c.hex_code
-        #FF051A
-        >>> str(c)
-        #FF051A
-        >>> int(c)
-        16712986
-        >>> c.rgb
-        (255, 5, 26)
-        >>> c.rgb_float
-        (1.0, 0.0196078431372549, 0.10196078431372549)
+    ```py
 
-    Alternatively, if you have an arbitrary input in one of the above formats that you wish to become a color, you can
-    use `Color.of` function on the class itself to automatically attempt to resolve the color:
+    >>> c = Color(0xFF051A)
+    Color(r=0xff, g=0x5, b=0x1a)
+    >>> hex(c)
+    0xff051a
+    >>> c.hex_code
+    #FF051A
+    >>> str(c)
+    #FF051A
+    >>> int(c)
+    16712986
+    >>> c.rgb
+    (255, 5, 26)
+    >>> c.rgb_float
+    (1.0, 0.0196078431372549, 0.10196078431372549)
+    ```
 
-        >>> Color.of(0xFF051A)
-        Color(r=0xff, g=0x5, b=0x1a)
-        >>> Color.of(16712986)
-        Color(r=0xff, g=0x5, b=0x1a)
-        >>> c = Color.of((255, 5, 26))
-        Color(r=0xff, g=0x5, b=1xa)
-        >>> c = Color.of(255, 5, 26)
-        Color(r=0xff, g=0x5, b=1xa)
-        >>> c = Color.of([0xFF, 0x5, 0x1a])
-        Color(r=0xff, g=0x5, b=1xa)
-        >>> c = Color.of("#1a2b3c")
-        Color(r=0x1a, g=0x2b, b=0x3c)
-        >>> c = Color.of("#1AB")
-        Color(r=0x11, g=0xaa, b=0xbb)
-        >>> c = Color.of((1.0, 0.0196078431372549, 0.10196078431372549))
-        Color(r=0xff, g=0x5, b=0x1a)
-        >>> c = Color.of([1.0, 0.0196078431372549, 0.10196078431372549])
-        Color(r=0xff, g=0x5, b=0x1a)
+    Alternatively, if you have an arbitrary input in one of the above formats
+    that you wish to become a color, you can use `Color.of` on the class itself
+    to automatically attempt to resolve the color:
+
+    ```py
+    >>> Color.of(0xFF051A)
+    Color(r=0xff, g=0x5, b=0x1a)
+    >>> Color.of(16712986)
+    Color(r=0xff, g=0x5, b=0x1a)
+    >>> c = Color.of((255, 5, 26))
+    Color(r=0xff, g=0x5, b=1xa)
+    >>> c = Color.of(255, 5, 26)
+    Color(r=0xff, g=0x5, b=1xa)
+    >>> c = Color.of([0xFF, 0x5, 0x1a])
+    Color(r=0xff, g=0x5, b=1xa)
+    >>> c = Color.of("#1a2b3c")
+    Color(r=0x1a, g=0x2b, b=0x3c)
+    >>> c = Color.of("#1AB")
+    Color(r=0x11, g=0xaa, b=0xbb)
+    >>> c = Color.of((1.0, 0.0196078431372549, 0.10196078431372549))
+    Color(r=0xff, g=0x5, b=0x1a)
+    >>> c = Color.of([1.0, 0.0196078431372549, 0.10196078431372549])
+    Color(r=0xff, g=0x5, b=0x1a)
+    ```
 
     Examples of initialization of Color objects from given formats include:
 
-        >>> c = Color(16712986)
-        Color(r=0xff, g=0x5, b=0x1a)
-        >>> c = Color.from_rgb(255, 5, 26)
-        Color(r=0xff, g=0x5, b=1xa)
-        >>> c = Color.from_hex_code("#1a2b3c")
-        Color(r=0x1a, g=0x2b, b=0x3c)
-        >>> c = Color.from_hex_code("#1AB")
-        Color(r=0x11, g=0xaa, b=0xbb)
-        >>> c = Color.from_rgb_float(1.0, 0.0196078431372549, 0.10196078431372549)
-        Color(r=0xff, g=0x5, b=0x1a)
+    ```py
+    >>> c = Color(16712986)
+    Color(r=0xff, g=0x5, b=0x1a)
+    >>> c = Color.from_rgb(255, 5, 26)
+    Color(r=0xff, g=0x5, b=1xa)
+    >>> c = Color.from_hex_code("#1a2b3c")
+    Color(r=0x1a, g=0x2b, b=0x3c)
+    >>> c = Color.from_hex_code("#1AB")
+    Color(r=0x11, g=0xaa, b=0xbb)
+    >>> c = Color.from_rgb_float(1.0, 0.0196078431372549, 0.10196078431372549)
+    Color(r=0xff, g=0x5, b=0x1a)
+    ```
     """
 
     __slots__ = ()
 
-    def __new__(cls, raw_rgb: typing.SupportsInt) -> Color:
+    def __new__(cls, raw_rgb: typing.Union[int, typing.SupportsInt], /) -> Color:
         if not 0 <= int(raw_rgb) <= 0xFF_FF_FF:
             raise ValueError(f"raw_rgb must be in the exclusive range of 0 and {0xFF_FF_FF}")
         return super(Color, cls).__new__(cls, raw_rgb)
@@ -119,6 +127,10 @@ class Color(int):
 
         Represented as a tuple of R, G, B. Each value is
         in the range [0, 0xFF].
+
+        Example
+        -------
+        `(123, 234, 47)`
         """
         return (self >> 16) & 0xFF, (self >> 8) & 0xFF, self & 0xFF
 
@@ -127,6 +139,10 @@ class Color(int):
         """Return the floating-point RGB representation of this Color.
 
         Represented as a tuple of R, G, B. Each value is in the range [0, 1].
+
+        Example
+        -------
+        `(0.1, 0.2, 0.76)`
         """
         r, g, b = self.rgb
         return r / 0xFF, g / 0xFF, b / 0xFF
@@ -137,8 +153,8 @@ class Color(int):
 
         This is prepended with a `#` symbol, and will be in upper case.
 
-        Examples
-        --------
+        Example
+        -------
         `#1A2B3C`
         """
         return "#" + self.raw_hex_code
@@ -147,8 +163,8 @@ class Color(int):
     def raw_hex_code(self) -> str:
         """Raw hex code.
 
-        Examples
-        --------
+        Example
+        -------
         `1A2B3C`
         """
         components = self.rgb
@@ -161,7 +177,7 @@ class Color(int):
         return not (((self & 0xFF0000) % 0x110000) or ((self & 0xFF00) % 0x1100) or ((self & 0xFF) % 0x11))
 
     @classmethod
-    def from_rgb(cls, red: int, green: int, blue: int) -> Color:
+    def from_rgb(cls, red: int, green: int, blue: int, /) -> Color:
         """Convert the given RGB to a `Color` object.
 
         Each channel must be withing the range [0, 255] (0x0, 0xFF).
@@ -195,19 +211,19 @@ class Color(int):
         return cls((red << 16) | (green << 8) | blue)
 
     @classmethod
-    def from_rgb_float(cls, red_f: float, green_f: float, blue_f: float) -> Color:
+    def from_rgb_float(cls, red: float, green: float, blue: float, /) -> Color:
         """Convert the given RGB to a `Color` object.
 
-        The colorspace represented values have to be within the
+        The color-space represented values have to be within the
         range [0, 1].
 
         Parameters
         ----------
-        red_f : float
+        red : float
             Red channel.
-        green_f : float
+        green : float
             Green channel.
-        blue_f : float
+        blue : float
             Blue channel.
 
         Returns
@@ -220,27 +236,28 @@ class Color(int):
         ValueError
             If red, green or blue are outside the range [0, 1].
         """
-        if not 0 <= red_f <= 1:
+        if not 0 <= red <= 1:
             raise ValueError("red must be in the inclusive range of 0 and 1.")
-        if not 0 <= green_f <= 1:
+        if not 0 <= green <= 1:
             raise ValueError("green must be in the inclusive range of 0 and 1.")
-        if not 0 <= blue_f <= 1:
+        if not 0 <= blue <= 1:
             raise ValueError("blue must be in the inclusive range of 0 and 1.")
         # noinspection PyTypeChecker
-        return cls.from_rgb(int(red_f * 0xFF), int(green_f * 0xFF), int(blue_f * 0xFF))
+        return cls.from_rgb(int(red * 0xFF), int(green * 0xFF), int(blue * 0xFF))
 
     @classmethod
-    def from_hex_code(cls, hex_code: str) -> Color:
+    def from_hex_code(cls, hex_code: str, /) -> Color:
         """Convert the given hexadecimal color code to a `Color`.
 
         The inputs may be of the following format (case insensitive):
-        `1a2`, `#1a2`, `0x1a2` (for websafe colors), or
+        `1a2`, `#1a2`, `0x1a2` (for web-safe colors), or
         `1a2b3c`, `#1a2b3c`, `0x1a2b3c` (for regular 3-byte color-codes).
 
         Parameters
         ----------
         hex_code : str
-            A hexadecimal color code to parse.
+            A hexadecimal color code to parse. This may optionally start with
+            a case insensitive `0x` or `#`.
 
         Returns
         -------
@@ -262,9 +279,9 @@ class Color(int):
 
         if len(hex_code) == 3:
             # Web-safe
-            components = (int(c, 16) for c in hex_code)
+            r, g, b = (c << 4 | c for c in (int(c, 16) for c in hex_code))
             # noinspection PyTypeChecker
-            return cls.from_rgb(*[(c << 4 | c) for c in components])
+            return cls.from_rgb(r, g, b)
 
         if len(hex_code) == 6:
             return cls.from_rgb(int(hex_code[:2], 16), int(hex_code[2:4], 16), int(hex_code[4:6], 16))
@@ -272,7 +289,7 @@ class Color(int):
         raise ValueError("Color code is invalid length. Must be 3 or 6 digits")
 
     @classmethod
-    def from_int(cls, i: typing.SupportsInt) -> Color:
+    def from_int(cls, i: typing.SupportsInt, /) -> Color:
         """Convert the given `typing.SupportsInt` to a `Color`.
 
         Parameters
@@ -326,10 +343,36 @@ class Color(int):
     ) -> Color:
         """Convert the value to a `Color`.
 
+        This attempts to determine the correct data format based on the
+        information provided.
+
         Parameters
         ----------
         values : ColorCompatibleT
-            A color comapible values.
+            A color compatible values.
+
+        Examples
+        --------
+        ```py
+        >>> Color.of(0xFF051A)
+        Color(r=0xff, g=0x5, b=0x1a)
+        >>> Color.of(16712986)
+        Color(r=0xff, g=0x5, b=0x1a)
+        >>> c = Color.of((255, 5, 26))
+        Color(r=0xff, g=0x5, b=1xa)
+        >>> c = Color.of(255, 5, 26)
+        Color(r=0xff, g=0x5, b=1xa)
+        >>> c = Color.of([0xFF, 0x5, 0x1a])
+        Color(r=0xff, g=0x5, b=1xa)
+        >>> c = Color.of("#1a2b3c")
+        Color(r=0x1a, g=0x2b, b=0x3c)
+        >>> c = Color.of("#1AB")
+        Color(r=0x11, g=0xaa, b=0xbb)
+        >>> c = Color.of((1.0, 0.0196078431372549, 0.10196078431372549))
+        Color(r=0xff, g=0x5, b=0x1a)
+        >>> c = Color.of([1.0, 0.0196078431372549, 0.10196078431372549])
+        Color(r=0xff, g=0x5, b=0x1a)
+        ```
 
         Returns
         -------
@@ -348,10 +391,12 @@ class Color(int):
                 raise ValueError(f"color must be an RGB triplet if set to a {type(values).__name__} type")
 
             if any(isinstance(c, float) for c in values):
-                return cls.from_rgb_float(*values)  # pylint: disable=no-value-for-parameter
+                r, g, b = values
+                return cls.from_rgb_float(r, g, b)
 
             if all(isinstance(c, int) for c in values):
-                return cls.from_rgb(*values)  # pylint: disable=no-value-for-parameter
+                r, g, b = values
+                return cls.from_rgb(r, g, b)
 
         if isinstance(values, str):
             is_start_hash_or_hex_literal = values.casefold().startswith(("#", "0x"))
