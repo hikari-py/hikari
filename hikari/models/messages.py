12a75bbda204534782a6ade8bbd6d59c3792eb62
davfsa <davfsa@gmail.com>
2020-07-18 23:08:39 +0200

    Implement message.link

--- hikari/models/messages.py
+++ hikari/models/messages.py
@@ -36,6 +36,7 @@ import typing
 import attr
 
 from hikari.utilities import files
+from hikari.utilities import constants
 from hikari.utilities import snowflake
 from hikari.utilities import undefined
 
@@ -330,6 +331,19 @@ class Message(snowflake.Unique):
 
     nonce: typing.Optional[str] = attr.ib(eq=False, hash=False, repr=False)
     """The message nonce. This is a string used for validating a message was sent."""
+
+    @property
+    def link(self) -> str:
+        """Jump link to the message.
+
+        Returns
+        -------
+        builtins.str
+            The jump link to the message.
+        """
+        if self.guild_id is None:
+            return f"{constants.BASE_URL}/channels/@me/{self.channel_id}/{self.id}"
+        return f"{constants.BASE_URL}/channels/{self.guild_id}/{self.channel_id}/{self.id}"
 
     async def fetch_channel(self) -> channels.PartialChannel:
         """Fetch the channel this message was created in.
