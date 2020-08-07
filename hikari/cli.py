995a8f72b96bbf308e9495e94f319aa71e55f80e
Nekokatt <3903853-nekokatt@users.noreply.gitlab.com>
2020-08-07 15:04:01 +0100

    Made banner and version API public, added git refs to versioning.

--- hikari/cli.py
+++ hikari/cli.py
@@ -26,15 +26,24 @@ import sys
 import typing
 
 from hikari import _about
+from hikari.utilities import art
 
 
 def main() -> None:
     """Print package info and exit."""
     # noinspection PyTypeChecker
-    sourcefile = typing.cast(str, inspect.getsourcefile(_about))
-    path: typing.Final[str] = os.path.abspath(os.path.dirname(sourcefile))
-    version: typing.Final[str] = _about.__version__
-    py_impl: typing.Final[str] = platform.python_implementation()
-    py_ver: typing.Final[str] = platform.python_version()
-    py_compiler: typing.Final[str] = platform.python_compiler()
-    sys.stderr.write(f"hikari v{version} (installed in {path}) ({py_impl} {py_ver} {py_compiler})\n")
+    if "--pretty" in sys.argv[1:] or "-p" in sys.argv[1:]:
+        sys.stdout.write(art.get_banner() + "\n")
+    else:
+        sourcefile = typing.cast(str, inspect.getsourcefile(_about))
+        path: typing.Final[str] = os.path.abspath(os.path.dirname(sourcefile))
+        branch: typing.Final[str] = _about.__git_branch__
+        sha1: typing.Final[str] = _about.__git_sha1__
+        date: typing.Final[str] = _about.__git_when__
+        version: typing.Final[str] = _about.__version__
+        py_impl: typing.Final[str] = platform.python_implementation()
+        py_ver: typing.Final[str] = platform.python_version()
+        py_compiler: typing.Final[str] = platform.python_compiler()
+        sys.stderr.write(f"hikari v{version} {branch}@{sha1}, released on {date}\n")
+        sys.stderr.write(f"located at {path}\n")
+        sys.stderr.write(f"{py_impl} {py_ver} {py_compiler}\n")
