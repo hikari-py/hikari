hikari-py/hikari

commit aa64c8ad531d3bce7a26f213422e5ae5ca260ec6

Rename `hikari.utitlities.date` to `hikari.utilities.time` (#239)

hikari/files.py
@@ -60,7 +60,7 @@ import aiohttp.client
 import attr
 
 from hikari.utilities import aio
-from hikari.utilities import date
+from hikari.utilities import time
 from hikari.utilities import net
 
 if typing.TYPE_CHECKING:
@@ -304,7 +304,7 @@ def generate_filename_from_details(
         extension = f".{extension}"
 
     # Nanosecond precision will be less likely to collide.
-    return date.uuid() + extension
+    return time.uuid() + extension
 
 
 def to_data_uri(data: bytes, mimetype: typing.Optional[str]) -> str:

hikari/impl/bot.py
@@ -60,7 +60,7 @@ from hikari.impl import rest as rest_impl
 from hikari.impl import shard as shard_impl
 from hikari.impl import voice as voice_impl
 from hikari.utilities import aio
-from hikari.utilities import date
+from hikari.utilities import time
 from hikari.utilities import event_stream
 from hikari.utilities import ux
 
@@ -761,7 +761,7 @@ class BotApp(traits.BotAware, event_dispatcher.EventDispatcher):
 
         # Dispatch the update checker, the sharding requirements checker, and dispatch
         # the starting event together to save a little time on startup.
-        start_time = date.monotonic()
+        start_time = time.monotonic()
 
         if check_for_updates:
             asyncio.create_task(
@@ -863,7 +863,7 @@ class BotApp(traits.BotAware, event_dispatcher.EventDispatcher):
 
         await self.dispatch(lifetime_events.StartedEvent(app=self))
 
-        _LOGGER.info("application started successfully in approx %.0f seconds", date.monotonic() - start_time)
+        _LOGGER.info("application started successfully in approx %.0f seconds", time.monotonic() - start_time)
 
     def stream(
         self,
@@ -947,9 +947,9 @@ class BotApp(traits.BotAware, event_dispatcher.EventDispatcher):
             url=url,
         )
 
-        start = date.monotonic()
+        start = time.monotonic()
         await aio.first_completed(new_shard.start(), self._closing_event.wait())
-        end = date.monotonic()
+        end = time.monotonic()
 
         if new_shard.is_alive:
             _LOGGER.debug("Shard %s started successfully in %.1fms", shard_id, (end - start) * 1_000)

hikari/impl/buckets.py
@@ -211,7 +211,7 @@ import typing
 
 from hikari.impl import rate_limits
 from hikari.utilities import aio
-from hikari.utilities import date
+from hikari.utilities import time
 from hikari.utilities import routes
 from hikari.utilities import ux
 
@@ -288,13 +288,13 @@ class RESTBucket(rate_limits.WindowedBurstRateLimiter):
             The epoch at which to reset the limit.
 
         !!! note
-            The `reset_at` epoch is expected to be a `date.monotonic_timestamp`
+            The `reset_at` epoch is expected to be a `time.monotonic_timestamp`
             monotonic epoch, rather than a `time.time` date-based epoch.
         """
         self.remaining = remaining
         self.limit = limit
         self.reset_at = reset_at
-        self.period = max(0.0, self.reset_at - date.monotonic())
+        self.period = max(0.0, self.reset_at - time.monotonic())
 
     def drip(self) -> None:
         """Decrement the remaining count for this bucket.
@@ -457,7 +457,7 @@ class RESTBucketManager:
         """
         buckets_to_purge = []
 
-        now = date.monotonic()
+        now = time.monotonic()
 
         # We have three main states that a bucket can be in:
         # 1. active - the bucket is active and is not at risk of deallocation
@@ -560,7 +560,7 @@ class RESTBucketManager:
         real_bucket_hash = compiled_route.create_real_bucket_hash(bucket_header)
 
         reset_after = (reset_at_header - date_header).total_seconds()
-        reset_at_monotonic = date.monotonic() + reset_after
+        reset_at_monotonic = time.monotonic() + reset_after
 
         if real_bucket_hash in self.real_hashes_to_buckets:
             bucket = self.real_hashes_to_buckets[real_bucket_hash]

hikari/impl/rest.py
@@ -71,7 +71,7 @@ from hikari.impl import rate_limits
 from hikari.impl import special_endpoints
 from hikari.impl import stateless_cache
 from hikari.utilities import data_binding
-from hikari.utilities import date
+from hikari.utilities import time
 from hikari.utilities import net
 from hikari.utilities import routes
 from hikari.utilities import ux
@@ -541,7 +541,7 @@ class RESTClientImpl(rest_api.RESTClient):
                 # Wait for any rate-limits to finish.
                 await asyncio.gather(self.buckets.acquire(compiled_route), self.global_rate_limit.acquire())
 
-                uuid = date.uuid()
+                uuid = time.uuid()
 
                 if _LOGGER.getEffectiveLevel() <= ux.TRACE:
                     _LOGGER.log(
@@ -555,7 +555,7 @@ class RESTClientImpl(rest_api.RESTClient):
 
                 # Make the request.
                 session = self._acquire_client_session()
-                start = date.monotonic()
+                start = time.monotonic()
                 response = await session.request(
                     compiled_route.method,
                     url,
@@ -568,7 +568,7 @@ class RESTClientImpl(rest_api.RESTClient):
                     proxy=self._proxy_settings.url,
                     proxy_headers=self._proxy_settings.all_headers,
                 )
-                time_taken = (date.monotonic() - start) * 1_000
+                time_taken = (time.monotonic() - start) * 1_000
 
                 if _LOGGER.getEffectiveLevel() <= ux.TRACE:
                     _LOGGER.log(
@@ -634,7 +634,7 @@ class RESTClientImpl(rest_api.RESTClient):
         reset_at = float(resp_headers.get(_X_RATELIMIT_RESET_HEADER, "0"))
         reset_after = float(resp_headers.get(_X_RATELIMIT_RESET_AFTER_HEADER, "0"))
         reset_date = datetime.datetime.fromtimestamp(reset_at, tz=datetime.timezone.utc)
-        now_date = date.rfc7231_datetime_string_to_datetime(resp_headers[_DATE_HEADER])
+        now_date = time.rfc7231_datetime_string_to_datetime(resp_headers[_DATE_HEADER])
 
         is_rate_limited = response.status == http.HTTPStatus.TOO_MANY_REQUESTS
 
@@ -777,7 +777,7 @@ class RESTClientImpl(rest_api.RESTClient):
         nsfw: undefined.UndefinedOr[bool] = undefined.UNDEFINED,
         bitrate: undefined.UndefinedOr[int] = undefined.UNDEFINED,
         user_limit: undefined.UndefinedOr[int] = undefined.UNDEFINED,
-        rate_limit_per_user: undefined.UndefinedOr[date.Intervalish] = undefined.UNDEFINED,
+        rate_limit_per_user: undefined.UndefinedOr[time.Intervalish] = undefined.UNDEFINED,
         permission_overwrites: undefined.UndefinedOr[
             typing.Sequence[channels.PermissionOverwrite]
         ] = undefined.UNDEFINED,
@@ -877,7 +877,7 @@ class RESTClientImpl(rest_api.RESTClient):
         self,
         channel: snowflakes.SnowflakeishOr[channels.GuildChannel],
         *,
-        max_age: undefined.UndefinedOr[date.Intervalish] = undefined.UNDEFINED,
+        max_age: undefined.UndefinedOr[time.Intervalish] = undefined.UNDEFINED,
         max_uses: undefined.UndefinedOr[int] = undefined.UNDEFINED,
         temporary: undefined.UndefinedOr[bool] = undefined.UNDEFINED,
         unique: undefined.UndefinedOr[bool] = undefined.UNDEFINED,
@@ -887,7 +887,7 @@ class RESTClientImpl(rest_api.RESTClient):
     ) -> invites.InviteWithMetadata:
         route = routes.POST_CHANNEL_INVITES.compile(channel=channel)
         body = data_binding.JSONObjectBuilder()
-        body.put("max_age", max_age, conversion=date.timespan_to_int)
+        body.put("max_age", max_age, conversion=time.timespan_to_int)
         body.put("max_uses", max_uses)
         body.put("temporary", temporary)
         body.put("unique", unique)
@@ -1735,7 +1735,7 @@ class RESTClientImpl(rest_api.RESTClient):
             guilds.GuildExplicitContentFilterLevel
         ] = undefined.UNDEFINED,
         afk_channel: undefined.UndefinedOr[snowflakes.SnowflakeishOr[channels.GuildVoiceChannel]] = undefined.UNDEFINED,
-        afk_timeout: undefined.UndefinedOr[date.Intervalish] = undefined.UNDEFINED,
+        afk_timeout: undefined.UndefinedOr[time.Intervalish] = undefined.UNDEFINED,
         icon: undefined.UndefinedNoneOr[files.Resourceish] = undefined.UNDEFINED,
         owner: undefined.UndefinedOr[snowflakes.SnowflakeishOr[users.PartialUser]] = undefined.UNDEFINED,
         splash: undefined.UndefinedNoneOr[files.Resourceish] = undefined.UNDEFINED,

hikari/impl/shard.py
@@ -46,7 +46,7 @@ from hikari import undefined
 from hikari.api import shard
 from hikari.impl import rate_limits
 from hikari.utilities import data_binding
-from hikari.utilities import date
+from hikari.utilities import time
 from hikari.utilities import net
 from hikari.utilities import ux
 
@@ -668,7 +668,7 @@ class GatewayShardImpl(shard.GatewayShard):
     async def _heartbeat(self, heartbeat_interval: float) -> bool:
         # Return True if zombied or should reconnect, false if time to die forever.
         # Prevent immediately zombie-ing.
-        self._last_heartbeat_ack_received = date.monotonic()
+        self._last_heartbeat_ack_received = time.monotonic()
         self._logger.debug("starting heartbeat with interval %ss", heartbeat_interval)
 
         while not self._closing.is_set() and not self._closed.is_set():
@@ -677,7 +677,7 @@ class GatewayShardImpl(shard.GatewayShard):
                 self._logger.warning(
                     "connection has not received a HEARTBEAT_ACK for approx %.1fs and is being disconnected, "
                     "expect a reconnect shortly",
-                    date.monotonic() - self._last_heartbeat_ack_received,
+                    time.monotonic() - self._last_heartbeat_ack_received,
                 )
                 return True
 
@@ -713,7 +713,7 @@ class GatewayShardImpl(shard.GatewayShard):
             await self._send_heartbeat_ack()
             self._logger.log(ux.TRACE, "sent HEARTBEAT")
         elif op == _HEARTBEAT_ACK:
-            now = date.monotonic()
+            now = time.monotonic()
             self._last_heartbeat_ack_received = now
             self._heartbeat_latency = now - self._last_heartbeat_sent
             self._logger.log(ux.TRACE, "received HEARTBEAT ACK in %.1fms", self._heartbeat_latency * 1_000)
@@ -756,7 +756,7 @@ class GatewayShardImpl(shard.GatewayShard):
 
         try:
             while not self._closing.is_set() and not self._closed.is_set():
-                if date.monotonic() - last_started_at < _BACKOFF_WINDOW:
+                if time.monotonic() - last_started_at < _BACKOFF_WINDOW:
                     time = next(backoff)
                     self._logger.info("backing off reconnecting for %.2fs", time)
 
@@ -769,7 +769,7 @@ class GatewayShardImpl(shard.GatewayShard):
                         pass
 
                 try:
-                    last_started_at = date.monotonic()
+                    last_started_at = time.monotonic()
                     should_restart = await self._run_once()
 
                     if not should_restart:
@@ -864,7 +864,7 @@ class GatewayShardImpl(shard.GatewayShard):
                 # This will currently allow us to try to resume if that happens
                 # We return True if zombied.
                 if await heartbeat_task:
-                    now = date.monotonic()
+                    now = time.monotonic()
                     self._logger.error(
                         "connection is a zombie, last heartbeat sent %.2fs ago",
                         now - self._last_heartbeat_sent,
@@ -901,7 +901,7 @@ class GatewayShardImpl(shard.GatewayShard):
 
     async def _send_heartbeat(self) -> None:
         await self._ws.send_json({_OP: _HEARTBEAT, _D: self._seq})  # type: ignore[union-attr]
-        self._last_heartbeat_sent = date.monotonic()
+        self._last_heartbeat_sent = time.monotonic()
 
     async def _send_heartbeat_ack(self) -> None:
         await self._ws.send_json({_OP: _HEARTBEAT_ACK, _D: None})  # type: ignore[union-attr]

hikari/impl/special_endpoints.py
@@ -43,7 +43,7 @@ from hikari import undefined
 from hikari.api import special_endpoints
 from hikari.utilities import attr_extensions
 from hikari.utilities import data_binding
-from hikari.utilities import date
+from hikari.utilities import time
 from hikari.utilities import routes
 
 if typing.TYPE_CHECKING:
@@ -314,7 +314,7 @@ class GuildBuilder(special_endpoints.GuildBuilder):
         *,
         parent_id: undefined.UndefinedOr[snowflakes.Snowflake] = undefined.UNDEFINED,
         topic: undefined.UndefinedOr[str] = undefined.UNDEFINED,
-        rate_limit_per_user: undefined.UndefinedOr[date.Intervalish] = undefined.UNDEFINED,
+        rate_limit_per_user: undefined.UndefinedOr[time.Intervalish] = undefined.UNDEFINED,
         position: undefined.UndefinedOr[int] = undefined.UNDEFINED,
         permission_overwrites: undefined.UndefinedOr[
             typing.Collection[channels.PermissionOverwrite]
@@ -327,7 +327,7 @@ class GuildBuilder(special_endpoints.GuildBuilder):
         payload.put("name", name)
         payload.put("type", channels.ChannelType.GUILD_TEXT)
         payload.put("topic", topic)
-        payload.put("rate_limit_per_user", rate_limit_per_user, conversion=date.timespan_to_int)
+        payload.put("rate_limit_per_user", rate_limit_per_user, conversion=time.timespan_to_int)
         payload.put("position", position)
         payload.put("nsfw", nsfw)
         payload.put_snowflake("parent_id", parent_id)

hikari/impl/stateful_guild_chunker.py
@@ -41,7 +41,7 @@ from hikari.api import chunker
 from hikari.events import shard_events
 from hikari.utilities import attr_extensions
 from hikari.utilities import collections
-from hikari.utilities import date
+from hikari.utilities import time
 from hikari.utilities import event_stream
 
 if typing.TYPE_CHECKING:
@@ -97,7 +97,7 @@ class ChunkStream(event_stream.EventStream[shard_events.MemberChunkEvent]):
         self._include_presences = include_presences
         self._limit = query_limit
         self._missing_chunks: typing.Optional[typing.MutableSequence[int]] = None
-        self._nonce = date.uuid()
+        self._nonce = time.uuid()
         self._query = query
         self._users = users
         self.filter(lambda event: event.nonce == self._nonce)
@@ -197,7 +197,7 @@ class _TrackedRequests:
         self.not_found_ids.extend(event.not_found)
         self.missing_chunk_indexes.remove(event.chunk_index)
         self._mono_last_received = time.monotonic_ns()
-        self.last_received = date.utc_datetime()
+        self.last_received = time.utc_datetime()
 
 
 class StatefulGuildChunkerImpl(chunker.GuildChunker):

tests/hikari/impl/test_buckets.py
@@ -26,7 +26,7 @@ import mock
 import pytest
 
 from hikari.impl import buckets
-from hikari.utilities import date as hikari_date
+from hikari.utilities import time as hikari_date
 from hikari.utilities import routes
 from tests.hikari import hikari_test_helpers
 

commit 8d256487fd8310b41ebb2220ca2da41f7726523b

Fix errors caused by #239

hikari/files.py
@@ -60,8 +60,8 @@ import aiohttp.client
 import attr
 
 from hikari.utilities import aio
-from hikari.utilities import time
 from hikari.utilities import net
+from hikari.utilities import time
 
 if typing.TYPE_CHECKING:
     import types

hikari/impl/bot.py
@@ -60,8 +60,8 @@ from hikari.impl import rest as rest_impl
 from hikari.impl import shard as shard_impl
 from hikari.impl import voice as voice_impl
 from hikari.utilities import aio
-from hikari.utilities import time
 from hikari.utilities import event_stream
+from hikari.utilities import time
 from hikari.utilities import ux
 
 if typing.TYPE_CHECKING:

hikari/impl/buckets.py
@@ -211,8 +211,8 @@ import typing
 
 from hikari.impl import rate_limits
 from hikari.utilities import aio
-from hikari.utilities import time
 from hikari.utilities import routes
+from hikari.utilities import time
 from hikari.utilities import ux
 
 if typing.TYPE_CHECKING:

hikari/impl/rest.py
@@ -71,9 +71,9 @@ from hikari.impl import rate_limits
 from hikari.impl import special_endpoints
 from hikari.impl import stateless_cache
 from hikari.utilities import data_binding
-from hikari.utilities import time
 from hikari.utilities import net
 from hikari.utilities import routes
+from hikari.utilities import time
 from hikari.utilities import ux
 
 if typing.TYPE_CHECKING:

hikari/impl/shard.py
@@ -46,8 +46,8 @@ from hikari import undefined
 from hikari.api import shard
 from hikari.impl import rate_limits
 from hikari.utilities import data_binding
-from hikari.utilities import time
 from hikari.utilities import net
+from hikari.utilities import time
 from hikari.utilities import ux
 
 if typing.TYPE_CHECKING:
@@ -757,11 +757,11 @@ class GatewayShardImpl(shard.GatewayShard):
         try:
             while not self._closing.is_set() and not self._closed.is_set():
                 if time.monotonic() - last_started_at < _BACKOFF_WINDOW:
-                    time = next(backoff)
-                    self._logger.info("backing off reconnecting for %.2fs", time)
+                    backoff_time = next(backoff)
+                    self._logger.info("backing off reconnecting for %.2fs", backoff_time)
 
                     try:
-                        await asyncio.wait_for(self._closing.wait(), timeout=time)
+                        await asyncio.wait_for(self._closing.wait(), timeout=backoff_time)
                         # We were told to close.
                         return
                     except asyncio.TimeoutError:

hikari/impl/special_endpoints.py
@@ -43,8 +43,8 @@ from hikari import undefined
 from hikari.api import special_endpoints
 from hikari.utilities import attr_extensions
 from hikari.utilities import data_binding
-from hikari.utilities import time
 from hikari.utilities import routes
+from hikari.utilities import time
 
 if typing.TYPE_CHECKING:
     import concurrent.futures

hikari/impl/stateful_guild_chunker.py
@@ -29,7 +29,6 @@ import asyncio
 import base64
 import copy
 import random
-import time
 import typing
 
 import attr
@@ -41,8 +40,8 @@ from hikari.api import chunker
 from hikari.events import shard_events
 from hikari.utilities import attr_extensions
 from hikari.utilities import collections
-from hikari.utilities import time
 from hikari.utilities import event_stream
+from hikari.utilities import time
 
 if typing.TYPE_CHECKING:
     import datetime
@@ -57,7 +56,7 @@ EXPIRY_TIME: typing.Final[int] = 5000
 
 
 def _random_nonce() -> str:
-    head = time.perf_counter_ns().to_bytes(8, "big")
+    head = time.monotonic_ns().to_bytes(8, "big")
     tail = random.getrandbits(92).to_bytes(12, "big")
     return base64.b64encode(head + tail).decode("ascii")
 

tests/hikari/impl/test_buckets.py
@@ -26,8 +26,8 @@ import mock
 import pytest
 
 from hikari.impl import buckets
-from hikari.utilities import time as hikari_date
 from hikari.utilities import routes
+from hikari.utilities import time as hikari_date
 from tests.hikari import hikari_test_helpers
 
 

tests/hikari/impl/test_shard.py
@@ -944,7 +944,7 @@ class TestGatewayShardImpl:
         client._closed = mock.Mock(is_set=mock.Mock(return_value=False))
         client._send_heartbeat = mock.AsyncMock()
 
-        with mock.patch.object(date, "monotonic", return_value=10):
+        with mock.patch.object(time, "monotonic", return_value=10):
             with mock.patch.object(asyncio, "wait_for", side_effect=[asyncio.TimeoutError, None]) as wait_for:
                 assert await client._heartbeat(20) is False
 
@@ -955,7 +955,7 @@ class TestGatewayShardImpl:
         client._last_heartbeat_sent = 10
         client._logger = mock.Mock()
 
-        with mock.patch.object(date, "monotonic", return_value=5):
+        with mock.patch.object(time, "monotonic", return_value=5):
             with mock.patch.object(asyncio, "wait_for") as wait_for:
                 assert await client._heartbeat(20) is True
 
@@ -988,7 +988,7 @@ class TestGatewayShardImpl:
         client._last_heartbeat_sent = 0
         client._seq = 10
 
-        with mock.patch.object(date, "monotonic", return_value=200):
+        with mock.patch.object(time, "monotonic", return_value=200):
             await client._send_heartbeat()
 
         client._ws.send_json.assert_awaited_once_with({"op": 1, "d": 10})

tests/hikari/impl/test_stateful_guild_chunker.py
@@ -20,7 +20,6 @@
 # SOFTWARE.
 import asyncio
 import copy
-import time
 
 import mock
 import pytest
@@ -36,6 +35,7 @@ from hikari.impl import shard
 from hikari.impl import stateful_guild_chunker
 from hikari.utilities import attr_extensions
 from hikari.utilities import event_stream
+from hikari.utilities import time
 from tests.hikari import hikari_test_helpers
 
 