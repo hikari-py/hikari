1d0561a8878dc414e8913df5d44f0adcb43c8858
Nekokatt <nekoka.tt@outlook.com>
2020-10-10 16:12:58 +0100

    Moved default Travis CI build stages to use CPython 3.9.0. (#280)
    * Moved default Travis CI build stages to use CPython 3.9.0.
    
    CPython 3.9.0 was released on Monday (two days ago, 5th October 2020).
    
    * Migrated test jobs to run within Docker to let me merge this issue quickly.
    
    * Added support for Python 3.10 dev
    
    * Fixed exit code in test scripts.
    
    * Disabled 3.10 CI until yarl and regex packages are upgraded.

---
+++ scripts/test-job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+
+if [ -z "${PYTHON_COMMAND:+x}" ]; then
+    export PYTHON_COMMAND=python
+fi
+
+mkdir public > /dev/null 2>&1 || true
+${PYTHON_COMMAND} -V
+${PYTHON_COMMAND} -m pip install nox
+${PYTHON_COMMAND} -m nox --sessions pytest
+${PYTHON_COMMAND} -m nox --sessions pytest-speedups -- --cov-append
+
+if [ "$(uname -s | perl -ne 'print lc')-$(uname -m)" = "linux-x86_64" ]; then
+    echo "Detected Linux AMD64, will upload coverage data"
+    curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+    chmod +x ./cc-test-reporter
+    ./cc-test-reporter after-build \
+        --exit-code 0 \
+        --id       "bf39911ceca45a536d408ea6456ed67460c73754f1411fb45f5e957398d98348"
+fi
