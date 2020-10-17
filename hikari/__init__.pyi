327b41265820136600ce4e0e6712f20d94efbeff
Forbidden-A <BluiePlayz@Yahoo.com>
2020-10-17 23:38:06 +0300

    Remove unexpected argument

---
+++ hikari/__init__.pyi
@@ -0,0 +1,31 @@
+from hikari.applications import *
+from hikari.audit_logs import *
+from hikari.channels import *
+from hikari.colors import *
+from hikari.colours import *
+from hikari.config import *
+from hikari.embeds import *
+from hikari.emojis import *
+from hikari.errors import *
+from hikari.events import *
+from hikari.guilds import *
+from hikari.intents import *
+from hikari.invites import *
+from hikari.iterators import *
+from hikari.messages import *
+from hikari.permissions import *
+from hikari.presences import *
+from hikari.sessions import *
+from hikari.traits import *
+from hikari.users import *
+from hikari.voices import *
+from hikari.webhooks import *
+from hikari._about import __author__ as __author__, __ci__ as __ci__, __copyright__ as __copyright__, __discord_invite__ as __discord_invite__, __docs__ as __docs__, __email__ as __email__, __issue_tracker__ as __issue_tracker__, __license__ as __license__, __url__ as __url__, __version__ as __version__
+from hikari.files import Bytes as Bytes, File as File, LazyByteIteratorish as LazyByteIteratorish, Pathish as Pathish, Rawish as Rawish, Resourceish as Resourceish, URL as URL
+from hikari.impl.bot import BotApp as BotApp
+from hikari.impl.rest import RESTApp as RESTApp
+from hikari.snowflakes import SearchableSnowflakeish as SearchableSnowflakeish, SearchableSnowflakeishOr as SearchableSnowflakeishOr, Snowflake as Snowflake, Snowflakeish as Snowflakeish, SnowflakeishOr as SnowflakeishOr, Unique as Unique
+from hikari.undefined import UNDEFINED as UNDEFINED, UndefinedNoneOr as UndefinedNoneOr, UndefinedOr as UndefinedOr, UndefinedType as UndefinedType
+from typing import Any
+
+__all__: Any
