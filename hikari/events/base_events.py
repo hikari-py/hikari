f6110cc89bef37217c4debc6cc4080444c9c0504
Faster Speeding <luke@lmbyrne.dev>
2021-06-02 11:36:11 +0100

    Add internal _FilteredMethod proto to event manager base * Move filtering to _handle_dispatch

--- hikari/events/base_events.py
+++ hikari/events/base_events.py
@@ -62,7 +62,7 @@ class Event(abc.ABC):
     def __init_subclass__(cls) -> None:
         super().__init_subclass__()
         # hasattr doesn't work with private variables in this case so we use a try except.
-        # We need to set Event's __dispatches when the first subclass is made as Event itself cannot
+        # We need to set Event's __dispatches when the first subclass is made as Event cannot
         # be included in a tuple literal on itself due to not existing yet.
         try:
             Event.__dispatches
