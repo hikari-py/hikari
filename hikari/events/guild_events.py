f03fac7fba298b37e71d6750acde1a55cf992b39
Ashwin Vinod <ashwinvinodsa@gmail.com>
2021-07-02 00:19:37 +0530

    God help me.

--- hikari/events/guild_events.py
+++ hikari/events/guild_events.py
@@ -372,7 +372,7 @@ class BanCreateEvent(BanEvent):
 
     user: users.User = attr.field()
     # <<inherited docstring from BanEvent>>.
-    
+
     @property
     def user_id(self) -> snowflakes.Snowflake:
         """User ID of the user that got banned.
@@ -384,7 +384,7 @@ class BanCreateEvent(BanEvent):
         """
         return self.user.id
 
-    async def fetch_ban(self) -> guilds.GuildMemberBan:
+    async def fetch_ban(self) -> guilds.GuildBan:
         """Perform an API call to fetch the details about this ban.
 
         This will include the optionally defined audit log reason for the
@@ -392,7 +392,7 @@ class BanCreateEvent(BanEvent):
 
         Returns
         -------
-        hikari.guilds.GuildMemberBan
+        hikari.guilds.GuildBan
             The ban details.
         """
         return await self.app.rest.fetch_ban(self.guild_id, self.user)
