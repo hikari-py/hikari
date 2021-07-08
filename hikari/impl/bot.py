a4fb97ab2617e5fca218902c08df00e3accb8201
davfsa <davfsa@gmail.com>
2021-07-08 19:17:04 +0200

    Update bot.py

--- hikari/impl/bot.py
+++ hikari/impl/bot.py
@@ -760,11 +760,8 @@ class GatewayBot(traits.GatewayBotAware):
                 name="check for package updates",
             )
 
-        requirements_task = asyncio.create_task(
-            self._rest.fetch_gateway_bot_info(), name="fetch gateway sharding settings"
-        )
         await self._event_manager.dispatch(self._event_factory.deserialize_starting_event())
-        requirements = await requirements_task
+        requirements = await self._rest.fetch_gateway_bot_info()
 
         if shard_count is None:
             shard_count = requirements.shard_count
