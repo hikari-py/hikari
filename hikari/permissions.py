f1a112e7a3e8aaa0cb6d83a02159a58a67612892
IkBenOlie5 <olivier@lombok.nl>
2021-08-19 22:53:21 +0200

    add has method to permissions

--- hikari/permissions.py
+++ hikari/permissions.py
@@ -270,6 +270,9 @@ class Permissions(enums.Flag):
     USE_PRIVATE_THREADS = 1 << 36
     """Allows for creating and participating in private threads."""
 
+    def has(self, permission: Permissions) -> bool:
+        return bool(self & permission)
+
     @classmethod
     def all_permissions(cls) -> Permissions:
         """Get an instance of `Permissions` with all the known permissions.
