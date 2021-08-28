57447b766e3e764599d1ea5868b2b33a2a8ab18e
vicky5124 <vickyf5124@gmail.com>
2021-08-28 15:48:14 +0200

    Added comments and formatter the code 	modified:   examples/simple_dashboard/bot.py

--- examples/simple_dashboard/bot.py
+++ examples/simple_dashboard/bot.py
@@ -9,15 +9,27 @@ import hikari
 
 PREFIX = ","
 
+# Name used to group dashboards.
+# You could have multiple packages for different applications, such as a package for the bot
+# dashboards, and another package for a web server running alongside the bot.
 PACKAGE = "Package"
+# Dashboards are a part inside of package, they can be used to group different types of
+# dashboards that you may want to use, like a dashboard for system status, another dashboard
+# for cache status, and another one to configure features or trigger actions on the bot.
 DASHBOARD = "Dashboard"
+# This are menus inside the dashboard, you can use them to group specific sets
+# of data inside the same dashboard.
 GROUP_CONFIG = "1 - Config"
+# All of the 3 configurable namescapes are sorted alphabetically.
 
 
 def is_command(cmd_name: str, content: str) -> bool:
     return content.startswith(f"{PREFIX}{cmd_name}")
 
 
+# This is how I do global data on the library, as Bot is generally accessible everywhere, so
+# it's the best you can do to put the global data in there.
+# In this case, I'll use this to store the values modifyable by the dashboard.
 class Data:
     def __init__(self) -> None:
         self.value = 0
@@ -33,17 +45,22 @@ class Bot(hikari.GatewayBot):
 # To see the dashboard, go to http://localhost:6361/ui/
 rillrate.install()
 bot = Bot(token=os.environ["DISCORD_TOKEN"], intents=hikari.Intents.ALL)
+# bot = Bot(token=os.environ["DISCORD_TOKEN"])
 
 
-values = []
-for i in range(0, 256 + 1):
-    if i % 32 == 0:
-        values.append(str(i))
+values = list(range(0, 256 + 1, 32))
 
-
-selector = rr_prime.Selector(f"{PACKAGE}.{DASHBOARD}.{GROUP_CONFIG}.Selector", label="Choose!", options=values)
+selector = rr_prime.Selector(
+    f"{PACKAGE}.{DASHBOARD}.{GROUP_CONFIG}.Selector",
+    label="Choose!",
+    options=values
+)
 slider = rr_prime.Slider(
-    f"{PACKAGE}.{DASHBOARD}.{GROUP_CONFIG}.Slider", label="More fine grain control", min=0, max=256, step=2
+    f"{PACKAGE}.{DASHBOARD}.{GROUP_CONFIG}.Slider",
+    label="More fine grain control",
+    min=0,
+    max=256,
+    step=2
 )
 
 
@@ -53,9 +70,11 @@ def selector_callback(activity: rillrate.Activity, action: rillrate.Action) -> N
     if action is not None:
         logging.info("Selected: %s", action.value)
 
+        # Update both the selector and slider, so they show with the same value.
         selector.apply(action.value)
         slider.apply(float(action.value))
 
+        # Overwrite the current stored value on the global data with the new selected value.
         bot.data.value = int(action.value)
 
 
@@ -68,6 +87,7 @@ def slider_callback(activity: rillrate.Activity, action: rillrate.Action) -> Non
         slider.apply(action.value)
         selector.apply(str(int(action.value)))
 
+        # Overwrite the current stored value on the global data with the new selected value.
         bot.data.value = int(action.value)
 
 
@@ -80,6 +100,7 @@ async def message(event: hikari.GuildMessageCreateEvent) -> None:
     if event.is_bot or not event.content:
         return
 
+    # Command Framework 101 :D
     if event.content.startswith(PREFIX):
         if is_command("ping", event.content):
             await event.message.respond("Ping?")
