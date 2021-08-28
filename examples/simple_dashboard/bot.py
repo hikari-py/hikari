c4df74e11c044356943a6c5580c312ac2c0be206
vicky5124 <vickyf5124@gmail.com>
2021-08-28 13:55:29 +0200

    Fixed mypy and updated to use constants 	modified:   examples/simple_dashboard/bot.py

--- examples/simple_dashboard/bot.py
+++ examples/simple_dashboard/bot.py
@@ -1,36 +1,53 @@
-import os
 import logging
+import os
+from typing import Any
 
-import hikari
 import rillrate
 from rillrate import prime as rr_prime
 
+import hikari
+
 PREFIX = ","
+
+PACKAGE = "Package"
+DASHBOARD = "Dashboard"
+GROUP_CONFIG = "1 - Config"
+
 
 def is_command(cmd_name: str, content: str) -> bool:
     return content.startswith(f"{PREFIX}{cmd_name}")
 
+
 class Data:
-    def __init__(self):
+    def __init__(self) -> None:
         self.value = 0
 
+
 class Bot(hikari.GatewayBot):
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
         self.data = Data()
 
+
+# Startup the dashboard.
+# To see the dashboard, go to http://localhost:6361/ui/
 rillrate.install()
 bot = Bot(token=os.environ["DISCORD_TOKEN"], intents=hikari.Intents.ALL)
 
+
 values = []
-for i in range(0, 256+1):
+for i in range(0, 256 + 1):
     if i % 32 == 0:
         values.append(str(i))
 
-selector = rr_prime.Selector("Package.Dashboard.1 - Config.Selector", label="Choose!", options=values)
-slider = rr_prime.Slider("Package.Dashboard.1 - Config.Slider", label="More fine grain control", min=0, max=256, step=2)
 
-def selector_callback(activity, action):
+selector = rr_prime.Selector(f"{PACKAGE}.{DASHBOARD}.{GROUP_CONFIG}.Selector", label="Choose!", options=values)
+slider = rr_prime.Slider(
+    f"{PACKAGE}.{DASHBOARD}.{GROUP_CONFIG}.Slider", label="More fine grain control", min=0, max=256, step=2
+)
+
+
+def selector_callback(activity: rillrate.Activity, action: rillrate.Action) -> None:
     logging.info("Selector activity: %s | action = %s", activity, action)
 
     if action is not None:
@@ -41,10 +58,8 @@ def selector_callback(activity, action):
 
         bot.data.value = int(action.value)
 
-selector.sync_callback(selector_callback)
 
-
-def slider_callback(activity, action):
+def slider_callback(activity: rillrate.Activity, action: rillrate.Action) -> None:
     logging.info("Slider activity: %s | action = %s", activity, action)
 
     if action is not None:
@@ -55,7 +70,10 @@ def slider_callback(activity, action):
 
         bot.data.value = int(action.value)
 
+
+selector.sync_callback(selector_callback)
 slider.sync_callback(slider_callback)
+
 
 @bot.listen()
 async def message(event: hikari.GuildMessageCreateEvent) -> None:
@@ -68,8 +86,10 @@ async def message(event: hikari.GuildMessageCreateEvent) -> None:
         elif is_command("value", event.content):
             await event.message.respond(f"Current value: {bot.data.value}")
 
+
 @bot.listen()
 async def on_ready(event: hikari.ShardReadyEvent) -> None:
     logging.info("Bot is ready! %s", event)
+
 
 bot.run()
