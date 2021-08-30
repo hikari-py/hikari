cc0caa216441202af4aa975055d773793eebcd08
Skelmis <skelmis.craft@gmail.com>
2021-08-30 19:49:04 +1200

    Add further text

--- hikari/migrating.py
+++ hikari/migrating.py
@@ -1,3 +1,8 @@
 """
 ## Migrating from discord.py to hikari, a sane beginners guide.
+
+Hikari is built around the idea of being performant and extendable.
+This means that it does not have a built in command handler, it lets
+you choose whichever route you wish to take. Due to this, command handling
+will be covered in the bottom part of this guide.
 """
