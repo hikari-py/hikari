891f8c2c1ad60f8ffa977358f152a56472815a4d
Skelmis <skelmis.craft@gmail.com>
2021-08-30 20:43:37 +1200

    Add some mappings for Users

--- hikari/migrating.py
+++ hikari/migrating.py
@@ -5,4 +5,12 @@ Hikari is built around the idea of being performant and extendable.
 This means that it does not have a built in command handler, it lets
 you choose whichever route you wish to take. Due to this, command handling
 will be covered in the bottom part of this guide.
+
+
+#### Users
+
+`discord.User` translates over to `hikari.users.User`
+
+The usage of which is fairly similar between packages with a notable exception
+being hikari uses `username` where discord.py simply uses `name`
 """
