8c8f8f9469b74010b1a45774e63462a02c9737da
davfsa <davfsa@gmail.com>
2022-02-02 18:25:45 +0100

    Fix `$browser` to use the correct format

--- hikari/impl/shard.py
+++ hikari/impl/shard.py
@@ -688,7 +688,7 @@ class GatewayShardImpl(shard.GatewayShard):
                 "large_threshold": self._large_threshold,
                 "properties": {
                     "$os": f"{platform.system()} {platform.architecture()[0]}",
-                    "$browser": f"aiohttp {aiohttp.__version__}",
+                    "$browser": f"hikari ({about.__version__}, aiohttp {aiohttp.__version__})",
                     "$device": f"hikari {about.__version__}",
                 },
                 "shard": [self._shard_id, self._shard_count],
