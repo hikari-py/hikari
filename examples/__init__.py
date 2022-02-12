ba05d6c6b0f8120f226c4fa01dda29b4218976ba
davfsa <davfsa@gmail.com>
2022-02-12 18:09:26 +0100

    Improve examples folder layout and cleanup rillrate example (#1019)

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,22 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2020 Nekokatt
-# Copyright (c) 2021-present davfsa
+# Hikari Examples - A collection of examples for Hikari.
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain worldwide.
+# This software is distributed without any warranty.
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# You should have received a copy of the CC0 Public Domain Dedication along with this software.
+# If not, see <https://creativecommons.org/publicdomain/zero/1.0/>.
 """Allows mypy to run here."""
---
+++ examples/hello_world.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Hikari Examples - A collection of examples for Hikari.
+#
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain worldwide.
+# This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along with this software.
+# If not, see <https://creativecommons.org/publicdomain/zero/1.0/>.
+"""A simple bot with only a `!ping` command."""
+
+import os
+
+import hikari
+
+bot = hikari.GatewayBot(token=os.environ["BOT_TOKEN"])
+
+
+@bot.listen()
+async def on_message(event: hikari.MessageCreateEvent) -> None:
+    """Listen for messages being created."""
+    if not event.is_human:
+        # Do not respond to bots or webhooks!
+        return
+
+    if event.content == "!ping":
+        await event.message.respond(f"Pong! {bot.heartbeat_latency * 1_000:.0f}ms")
+
+
+bot.run()
