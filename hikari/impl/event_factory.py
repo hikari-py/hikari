cf74dfffa907b3d9bcf324f2677bf008fcfcec87
Lucina <lucina@lmbyrne.dev>
2022-03-26 00:57:21 +0000

    Remove redundant event factory assertions (#1105)

--- hikari/impl/event_factory.py
+++ hikari/impl/event_factory.py
@@ -210,10 +210,6 @@ class EventFactoryImpl(event_factory.EventFactory):
         self, shard: gateway_shard.GatewayShard, payload: data_binding.JSONObject
     ) -> guild_events.GuildJoinEvent:
         guild_information = self._app.entity_factory.deserialize_gateway_guild(payload)
-        assert guild_information.channels is not None
-        assert guild_information.members is not None
-        assert guild_information.presences is not None
-        assert guild_information.voice_states is not None
         return guild_events.GuildJoinEvent(
             shard=shard,
             guild=guild_information.guild(),
