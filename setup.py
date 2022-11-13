910d241a99f4fb5bce5d22bf227cb682ebabc373
Faster Speeding <luke@lmbyrne.dev>
2022-11-13 09:36:38 +0000

    Make sure this stuff doesn't get missed when this is actually merged

--- setup.py
+++ setup.py
@@ -95,6 +95,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        # "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Communications :: Chat",
         "Topic :: Internet :: WWW/HTTP",
