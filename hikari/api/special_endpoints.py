050d2143a00814b764fbb77a94c6951dd4e2e309
Lucina <lucina@lmbyrne.dev>
2023-02-13 17:59:56 +0000

    Remove redundant abc method

--- hikari/api/special_endpoints.py
+++ hikari/api/special_endpoints.py
@@ -1488,11 +1488,6 @@ class SelectMenuBuilder(ComponentBuilder, abc.ABC, typing.Generic[_ContainerT]):
 
     @property
     @abc.abstractmethod
-    def type(self) -> typing.Union[int, components_.ComponentType]:
-        """Type of the select menu this builds."""
-
-    @property
-    @abc.abstractmethod
     def custom_id(self) -> str:
         """Developer set custom ID used for identifying interactions with this menu."""
 
