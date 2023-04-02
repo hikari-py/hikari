30d7593261f3ad04280188e995f7d06bec08f060
Lucina <lucina@lmbyrne.dev>
2023-04-02 03:00:53 +0100

    Update rest.py

--- hikari/api/rest.py
+++ hikari/api/rest.py
@@ -1081,11 +1081,11 @@ class RESTClient(traits.NetworkSettingsAware, abc.ABC):
         embeds : hikari.undefined.UndefinedOr[typing.Sequence[hikari.embeds.Embed]]
             If provided, the message embeds.
         sticker : hikari.undefined.UndefinedOr[hikari.snowflakes.SnowflakeishOr[hikari.stickers.PartialSticker]]
-            If provided, object or ID of a sticker to send on the message.
+            If provided, the object or ID of a sticker to send on the message.
 
             As of writing, bots can only send custom stickers from the current guild.
         stickers : hikari.undefined.UndefinedOr[hikari.snowflakes.SnowflakeishSequence[hikari.stickers.PartialSticker]]
-            If provided, object or ID of up to 3 stickers to send on the message.
+            If provided, a sequence of objects and IDs of up to 3 stickers to send on the message.
 
             As of writing, bots can only send custom stickers from the current guild.
         tts : hikari.undefined.UndefinedOr[bool]
@@ -4716,11 +4716,11 @@ class RESTClient(traits.NetworkSettingsAware, abc.ABC):
         embeds : hikari.undefined.UndefinedOr[typing.Sequence[hikari.embeds.Embed]]
             If provided, the message embeds.
         sticker : hikari.undefined.UndefinedOr[hikari.snowflakes.SnowflakeishOr[hikari.stickers.PartialSticker]]
-            If provided, object or ID of a sticker to send on the message.
+            If provided, the object or ID of a sticker to send on the message.
 
             As of writing, bots can only send custom stickers from the current guild.
         stickers : hikari.undefined.UndefinedOr[hikari.snowflakes.SnowflakeishSequence[hikari.stickers.PartialSticker]]
-            If provided, object or ID of up to 3 stickers to send on the message.
+            If provided, a sequence of objects and IDs of up to 3 stickers to send on the message.
 
             As of writing, bots can only send custom stickers from the current guild.
         tts : hikari.undefined.UndefinedOr[bool]
