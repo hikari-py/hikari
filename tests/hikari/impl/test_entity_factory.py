71031c26814eab8afd378b0149e738907a721612
Nick <83879796+sdf9s8d76f@users.noreply.github.com>
2023-09-23 16:30:13 +0200

    add new line
    Signed-off-by: Nick <83879796+sdf9s8d76f@users.noreply.github.com>

--- tests/hikari/impl/test_entity_factory.py
+++ tests/hikari/impl/test_entity_factory.py
@@ -886,6 +886,7 @@ class TestEntityFactoryImpl:
         assert own_connection.is_activity_visible is True
         assert own_connection.visibility == application_models.ConnectionVisibility.NONE
         assert isinstance(own_connection, application_models.OwnConnection)
+
     @pytest.fixture()
     def own_guild_payload(self):
         return {
