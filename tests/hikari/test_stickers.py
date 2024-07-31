2bb86d85ff7d722539dff09c86200918466e1341
davfsa <davfsa@gmail.com>
2024-07-31 23:45:51 +0200

    Use Discord Media Proxy instead of CDN for animated stickers urls (#1982)

--- tests/hikari/test_stickers.py
+++ tests/hikari/test_stickers.py
@@ -85,3 +85,13 @@ class TestPartialSticker:
             assert model.image_url == "file"
 
         route.compile_to_file.assert_called_once_with(urls.CDN_URL, sticker_id=123, file_format="json")
+
+    def test_image_url_when_GIF_uses_media_proxy(self, model):
+        model.format_type = stickers.StickerFormatType.GIF
+
+        with mock.patch.object(
+            routes, "CDN_STICKER", new=mock.Mock(compile_to_file=mock.Mock(return_value="file"))
+        ) as route:
+            assert model.image_url == "file"
+
+        route.compile_to_file.assert_called_once_with(urls.MEDIA_PROXY_URL, sticker_id=123, file_format="gif")
