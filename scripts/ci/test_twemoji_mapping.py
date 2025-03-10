ece746be9570f9e9fe224516f2392142762592f4
davfsa <davfsa@gmail.com>
2025-03-10 01:49:54 +0100

    Update twemoji mapping url

--- scripts/ci/test_twemoji_mapping.py
+++ scripts/ci/test_twemoji_mapping.py
@@ -38,7 +38,7 @@ sys.path.append("..")
 from hikari import emojis
 
 TWEMOJI_REPO_BASE_URL = "https://github.com/discord/twemoji.git"
-DISCORD_EMOJI_MAPPING_URL = "https://emzi0767.gl-pages.emzi0767.dev/discord-emoji/discordEmojiMap-canary.min.json"
+DISCORD_EMOJI_MAPPING_URL = "https://emzi0767.mzgit.io/discord-emoji/discordEmojiMap-canary.min.json"
 
 try:
     tempdir = pathlib.Path(sys.argv[1])
