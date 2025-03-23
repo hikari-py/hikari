5a6758370ecef811bf734467f45edad84f4a31a4
Faster Speeding <lucina@lmbyrne.dev>
2025-03-23 12:02:24 +0000

    Small fix

--- hikari/internal/override.py
+++ hikari/internal/override.py
@@ -19,7 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-"""Deprecation utils."""
+"""@typing.override dummy function."""
 
 from __future__ import annotations
 
