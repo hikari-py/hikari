97df56c763ca7ca1bc8d2a5f787bf91619df634f
schoolbusgaming <spammly2.0@gmail.com>
2025-06-01 09:52:56 +1200

    Refactor emoji classes and methods
    Simplified docstrings, removed redundant annotations, and streamlined method implementations in `hikari/emojis.py`. This refactor improves readability and reduces unnecessary complexity in the emoji-related classes and methods.

--- hikari/emojis.py
+++ hikari/emojis.py
@@ -50,8 +50,6 @@ class Emoji(files.WebResource, abc.ABC):
 
     Any emoji implementation supports being used as a
     [`hikari.files.Resource`][] when uploading an attachment to the API.
-    This is achieved in the same way as using a
-    [`hikari.files.WebResource`][] would achieve this.
     """
 
     __slots__: typing.Sequence[str] = ()
@@ -63,7 +61,6 @@ class Emoji(files.WebResource, abc.ABC):
 
     @property
     @abc.abstractmethod
-    @typing_extensions.override
     def url(self) -> str:
         """URL of the emoji image to display in clients."""
 
@@ -78,19 +75,10 @@ class Emoji(files.WebResource, abc.ABC):
         """Mention string to use to mention the emoji with."""
 
     @classmethod
-    def parse(cls, string: str, /) -> UnicodeEmoji | CustomEmoji:
+    def parse(cls, string: str) -> "UnicodeEmoji | CustomEmoji":
         """Parse a given string into an emoji object.
 
-        Parameters
-        ----------
-        string
-            The emoji object to parse.
-
-        Returns
-        -------
-        typing.Union[UnicodeEmoji, CustomEmoji]
-            The parsed emoji object. This will be a [`hikari.emojis.CustomEmoji`][] if a custom
-            emoji mention, or a [`hikari.emojis.UnicodeEmoji`][] otherwise.
+        Returns a [`CustomEmoji`][] if a custom emoji mention, or a [`UnicodeEmoji`][] otherwise.
 
         Raises
         ------
@@ -103,230 +91,108 @@ class Emoji(files.WebResource, abc.ABC):
 
 
 class UnicodeEmoji(str, Emoji):
-    """Represents a unicode emoji.
-
-    !!! warning
-        A word of warning if you try to upload this emoji as a file attachment.
-
-        While this emoji type can be used to upload the Twemoji representations
-        of this emoji as a PNG, this is NOT foolproof. The mapping between
-        Discord's implementation and official Twemoji bindings is very flaky.
-        Responsible implementations relying on this behaviour will be
-        implemented to expect this behaviour in the form of
-        [`hikari.errors.NotFoundError`][] exceptions being raised when a mismatch may
-        occur. It is also likely that this will change in the future without
-        notice, so you will likely be relying on flaky behaviour.
-
-        If this is proven to be too unstable, this functionality will be
-        removed in a future release after a deprecation period.
-    """
+    """Represents a unicode emoji."""
 
     __slots__: typing.Sequence[str] = ()
 
     @property
-    @typing_extensions.override
     def name(self) -> str:
         """Return the code points which form the emoji."""
         return self
 
     @property
-    @typing.final
-    @typing_extensions.override
     def url_name(self) -> str:
         return self
 
     @property
-    @typing_extensions.override
     def mention(self) -> str:
         return self
 
     @property
-    @typing.final
     def codepoints(self) -> typing.Sequence[int]:
         """Integer codepoints that make up this emoji, as UTF-8."""
         return [ord(c) for c in self]
 
     @property
-    @typing_extensions.override
     def filename(self) -> str:
         """Filename to use if re-uploading this emoji's PNG."""
         codepoints = self.codepoints
-        # It looks like the rule is to delete character #2 if the value
-        # of this is 0xfe0f and the character is up to 4 characters long.
-        # Other than that, the mapping is 1-to-1. There's a couple of outliers
-        # to this 0xfe0f rule and they seem to follow a pattern where the
-        # codepoint after 0xfe0f is 0x200D but this might be a coincidence and
-        # if Discord start breaking this regularly I might need to ask for a
-        # more permanent solution. This is provisionally provided. If we find it
-        # breaks in other ways, I will just revoke this functionality in a
-        # future update.
+        # Remove 0xfe0f if present as the second character and not followed by 0x200D
         if codepoints[1:2] == [0xFE0F] and len(codepoints) <= 4 and codepoints[2:3] != [0x200D]:
             codepoints = [codepoints[0], *codepoints[2:]]
-
-        return "-".join(hex(c)[2:] for c in codepoints) + ".png"
+        return "-".join(f"{c:x}" for c in codepoints) + ".png"
 
     @property
-    @typing_extensions.override
     def url(self) -> str:
-        """Get the URL of the PNG rendition of this emoji.
-
-        This will use the official Twitter "twemoji" repository to fetch
-        this information, as Discord only stores this in a hashed format
-        that uses SVG files, which is not usually of any use.
-
-        Since this uses "twemoji" directly, the emojis may not directly
-        match what is on Discord if Discord have failed to keep their emoji
-        packs up-to-date with this repository.
-
-        Examples
-        --------
-        ```py
-        >>> emoji = hikari.UnicodeEmoji("\N{OK HAND SIGN}")
-        >>> emoji.url
-        'https://raw.githubusercontent.com/discord/twemoji/master/assets/72x72/1f44c.png'
-        ```
-        """
-        return _TWEMOJI_PNG_BASE_URL + self.filename
+        """Get the URL of the PNG rendition of this emoji."""
+        return f"{_TWEMOJI_PNG_BASE_URL}{self.filename}"
 
     @property
-    @typing.final
     def unicode_escape(self) -> str:
         """Get the unicode escape string for this emoji."""
         return bytes(self, "unicode_escape").decode("utf-8")
 
     @classmethod
-    @typing.final
-    def parse_codepoints(cls, codepoint: int, *codepoints: int) -> UnicodeEmoji:
+    def parse_codepoints(cls, codepoint: int, *codepoints: int) -> "UnicodeEmoji":
         """Create a unicode emoji from one or more UTF-32 codepoints."""
         return cls("".join(map(chr, (codepoint, *codepoints))))
 
     @classmethod
-    @typing.final
-    def parse_unicode_escape(cls, escape: str) -> UnicodeEmoji:
+    def parse_unicode_escape(cls, escape: str) -> "UnicodeEmoji":
         """Create a unicode emoji from a unicode escape string."""
         return cls(escape.encode("utf-8"), "unicode_escape")
 
     @classmethod
-    @typing.final
-    @typing_extensions.override
-    def parse(cls, string: str, /) -> UnicodeEmoji:
-        """Parse a given string into a unicode emoji object.
-
-        Parameters
-        ----------
-        string
-            The emoji object to parse.
-
-        Returns
-        -------
-        UnicodeEmoji
-            The parsed UnicodeEmoji object.
-        """
+    def parse(cls, string: str) -> "UnicodeEmoji":
+        """Parse a given string into a unicode emoji object."""
         # TODO: Add validity check here (maybe use optional discord_emojis package)
-
         return cls(string)
 
 
 @attrs_extensions.with_copy
 @attrs.define(unsafe_hash=True, kw_only=True, weakref_slot=False)
 class CustomEmoji(snowflakes.Unique, Emoji):
-    """Represents a custom emoji.
-
-    This is a custom emoji that is from a guild you might not be part of.
-
-    All CustomEmoji objects and their derivatives act as valid
-    [`hikari.files.Resource`][] objects. This means you can use them as a
-    file when sending a message.
-
-        >>> emojis = await bot.rest.fetch_guild_emojis(12345)
-        >>> picks = random.choices(emojis, 5)
-        >>> await event.respond(files=picks)
-
-    !!! warning
-        Discord will not provide information on whether these emojis are
-        animated or not when a reaction is removed and an event is fired. This
-        is problematic if you need to try and determine the emoji that was
-        removed. The side effect of this means that mentions for animated emojis
-        will not be correct.
-
-        This will not be changed as stated here:
-        <https://github.com/discord/discord-api-docs/issues/1614#issuecomment-628548913>
-    """
+    """Represents a custom emoji."""
 
     id: snowflakes.Snowflake = attrs.field(hash=True, repr=True)
-    """The ID of this entity."""
-
     name: str = attrs.field(eq=False, hash=False, repr=True)
-    """The name of the emoji."""
-
     is_animated: bool = attrs.field(eq=False, hash=False, repr=True)
-    """Whether the emoji is animated."""
 
-    @typing_extensions.override
     def __str__(self) -> str:
         return self.mention
 
-    @typing_extensions.override
     def __eq__(self, other: object) -> bool:
         if isinstance(other, CustomEmoji):
             return self.id == other.id
-
         return False
 
     @property
-    @typing_extensions.override
     def filename(self) -> str:
-        return str(self.id) + (".gif" if self.is_animated else ".png")
+        return f"{self.id}.{'gif' if self.is_animated else 'png'}"
 
     @property
-    @typing.final
-    @typing_extensions.override
     def url_name(self) -> str:
         return f"{self.name}:{self.id}"
 
     @property
-    @typing.final
-    @typing_extensions.override
     def mention(self) -> str:
         return f"<{'a' if self.is_animated else ''}:{self.url_name}>"
 
     @property
-    @typing.final
-    @typing_extensions.override
     def url(self) -> str:
         ext = "gif" if self.is_animated else "png"
-
         return routes.CDN_CUSTOM_EMOJI.compile(urls.CDN_URL, emoji_id=self.id, file_format=ext)
 
     @classmethod
-    @typing_extensions.override
-    def parse(cls, string: str, /) -> CustomEmoji:
-        """Parse a given emoji mention string into a custom emoji object.
-
-        Parameters
-        ----------
-        string
-            The emoji mention to parse.
-
-        Returns
-        -------
-        CustomEmoji
-            The parsed emoji object.
-
-        Raises
-        ------
-        ValueError
-            If a mention is given that has an invalid format.
-        """
+    def parse(cls, string: str) -> "CustomEmoji":
+        """Parse a given emoji mention string into a custom emoji object."""
         if emoji_match := _CUSTOM_EMOJI_REGEX.match(string):
             return CustomEmoji(
                 id=snowflakes.Snowflake(emoji_match.group("id")),
                 name=emoji_match.group("name"),
                 is_animated=emoji_match.group("flags").lower() == "a",
             )
-
-        msg = "Expected an emoji mention"
-        raise ValueError(msg)
+        raise ValueError("Expected an emoji mention")
 
 
 @attrs.define(unsafe_hash=True, kw_only=True, weakref_slot=False)
