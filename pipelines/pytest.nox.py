285d6c675a548bfd6e25bc451ee4cb426cb133d2
davfsa <davfsa@gmail.com>
2025-09-24 06:45:07 +0000

    chore: reenable pytest-all-features in cpython 3.14 (#2548)

--- pipelines/pytest.nox.py
+++ pipelines/pytest.nox.py
@@ -22,7 +22,6 @@
 
 from __future__ import annotations
 
-import sys
 import typing
 
 from pipelines import config
@@ -58,10 +57,6 @@ def pytest_all_features(session: nox.Session) -> None:
 
     Coverage can be enabled with the `--coverage` flag.
     """
-    if sys.version_info >= (3, 14):
-        session.log("pytest-all-features is disabled in 3.14+ due to https://github.com/pytest-dev/pytest/issues/13739")
-        return
-
     _pytest(session, extras_install=["speedups", "server"], python_flags=("-OO",))
 
 
