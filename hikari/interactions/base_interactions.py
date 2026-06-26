69fedbdd6a02cc3ab0f13e94de7abef3768b3b03
Platy <contact@mplaty.com>
2026-06-27 01:36:30 +1000

    Update hikari/interactions/base_interactions.py
    Co-authored-by: ankar-dav <dfsanguino@ankar.ai>
    Signed-off-by: Platy <contact@mplaty.com>

--- hikari/interactions/base_interactions.py
+++ hikari/interactions/base_interactions.py
@@ -340,7 +340,7 @@ class PartialInteraction(snowflakes.Unique, webhooks.ExecutableWebhook):
     """For monetized apps, any entitlements for the invoking user, represents access to SKUs."""
 
     attachment_size_limit: int = attrs.field(eq=False, hash=False, repr=True)
-    """The size limit of the attachments for both the bot and user."""
+    """The size limit of the attachments that can be uploaded."""
 
     @property
     def channel_id(self) -> snowflakes.Snowflake:
