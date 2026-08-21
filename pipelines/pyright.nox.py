c978da2fbd19a2b38bc3889416abe8132712f2d1
Christopher <github.android206@passmail.net>
2026-08-21 14:33:56 +0200

    Revert "I think this can be removed right?"
    This reverts commit a9174be04e23fd170f58aba416136474e141650f.

--- pipelines/pyright.nox.py
+++ pipelines/pyright.nox.py
@@ -28,7 +28,12 @@ from pipelines import nox
 
 @nox.session()
 def pyright(session: nox.Session) -> None:
-    """Perform static type analysis on Python source code using pyright."""
+    """Perform static type analysis on Python source code using pyright.
+
+    At the time of writing this, this pipeline will not run successfully,
+    as hikari does not have 100% compatibility with pyright just yet. This
+    exists to make it easier to test and eventually reach that 100% compatibility.
+    """
     nox.sync(session, self=True, extras=["speedups", "server"], groups=["pyright"])
     session.run("pyright")
 
